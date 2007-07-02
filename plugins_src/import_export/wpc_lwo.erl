3f568971ecfdc9b1f1edeb2b5025e992ef9880b1
Anthony D'Agostino <antoneos@users.sourceforge.net>
2007-07-02 18:31:49 +0000

    Set default distance/rotation constraint for pressed modifier key(s).
    git-svn-id: https://wings.svn.sourceforge.net/svnroot/wings/trunk@96 a0a82711-911a-0410-9f52-85ee3e23afd8

--- plugins_src/import_export/wpc_lwo.erl
+++ plugins_src/import_export/wpc_lwo.erl
@@ -8,7 +8,7 @@
 %%  See the file "license.terms" for information on usage and redistribution
 %%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 %%
-%%     $Id: wpc_lwo.erl,v 1.1 2006/09/06 22:50:49 antoneos Exp $
+%%     $Id$
 %%
 
 -module(wpc_lwo).
