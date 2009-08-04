dgud/wings

commit ddad5076ed6a36b6989d57e9b73681a2cc2269d6

Rewrite AutoUV to use wings_va

plugins_src/autouv/wpc_snap.erl
@@ -5,7 +5,7 @@
 %%%
 %%% Created : 28 May 2003 by Dan Gudmundsson
 %%-------------------------------------------------------------------
-%%  Copyright (c) 2002-2004 Dan Gudmundsson, Bjorn Gustavsson
+%%  Copyright (c) 2002-2009 Dan Gudmundsson, Bjorn Gustavsson
 %%
 %%  See the file "license.terms" for information on usage and redistribution
 %%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
@@ -255,27 +255,26 @@ scale(W,H,IW,IH) ->
  	    end
     end.
 
-update_uv_fun(Vs) ->
-    fun(Face, _V, Edge, Rec0, E0) ->
+update_uv_fun(Vtab) ->
+    fun(Face, V, Edge, Rec0, We) ->
 	    case Rec0 of
 		#edge{vs=V,lf=Face} ->
-		    Rec  = array:get(Edge, E0),
-		    Vpos = array:get(V, Vs),
+		    Vpos = array:get(V, Vtab),
 		    UV = calc_uv(Vpos),
-		    array:set(Edge, Rec#edge{a=UV},E0);
+		    Attr = wings_va:new_attr(uv, UV),
+		    wings_va:set_edge_attrs(Edge, left, Attr, We);
 		#edge{ve=V,rf=Face} ->
-		    Rec  = array:get(Edge, E0),
-		    Vpos = array:get(V, Vs),
+		    Vpos = array:get(V, Vtab),
 		    UV = calc_uv(Vpos),
-		    array:set(Edge, Rec#edge{b=UV},E0)
+		    Attr = wings_va:new_attr(uv, UV),
+		    wings_va:set_edge_attrs(Edge, right, Attr, We)
 	    end
     end.
 
 insert_uvs(St0) ->
-    wings_sel:map(fun(Items, We=#we{vp=Vs,es=Etab0}) ->  
-			  AddUv = update_uv_fun(Vs),
-			  Etab = wings_face:fold_faces(AddUv, Etab0, Items, We),
-			  We#we{es=Etab}
+    wings_sel:map(fun(Items, We=#we{vp=Vtab}) ->
+			  AddUv = update_uv_fun(Vtab),
+			  wings_face:fold_faces(AddUv, We, Items, We)
 		  end, St0).
 
 set_materials(Image,St0) ->     

plugins_src/autouv/auv_segment.erl
@@ -694,11 +694,11 @@ map_edge(E0, Emap) ->
 %%% Cutting along hard edges.
 %%%
 
-cut_model(Charts, Cuts0, #we{es=Etab0}=We0) ->
-    Etab = array:sparse_map(fun(_, Rec) ->
-				    Rec#edge{a=none,b=none}
-			    end, Etab0),
-    We = We0#we{mode=material,es=Etab},
+cut_model(Charts, Cuts0, #we{es=Etab}=We0) ->
+    Clean = fun(Edge, _Rec, W) ->
+		    wings_va:set_both_edge_attrs(Edge, none, none, W)
+	    end,
+    We = array:sparse_foldl(Clean, We0#we{mode=material}, Etab),
     Cuts = gb_sets:to_list(Cuts0),
     cut_model_1(Charts, Cuts, We#we{mirror=none}, length(Charts), []).
 

plugins_src/autouv/wpc_autouv.erl
@@ -330,11 +330,7 @@ update_selected_uvcoords(#st{bb=Uvs}=St) ->
 
 %% update_uvs(Charts, We0) -> We
 %%  Update the UV coordinates for the original model.
-update_uvs(Cs, #we{es=Etab0}=GeomWe) ->
-    update_uvs_1(Cs, GeomWe, Etab0).
-
-update_uvs_1([#we{vp=Vpos0,name=#ch{vmap=Vmap}}=ChartWe|Cs],
-	     We, Etab0) ->
+update_uvs([#we{vp=Vpos0,name=#ch{vmap=Vmap}}=ChartWe|Cs], We0) ->
     VFace0 = wings_face:fold_faces(
 	       fun(Face, V, _, _, A) ->
 		       [{V,Face}|A]
@@ -344,38 +340,20 @@ update_uvs_1([#we{vp=Vpos0,name=#ch{vmap=Vmap}}=ChartWe|Cs],
     VFace2 = sofs:relation_to_family(VFace1),
     VFace = sofs:to_external(VFace2),
     Vpos = array:sparse_to_orddict(Vpos0),
-    Etab = update_uvs_2(Vpos, VFace, Vmap, We, Etab0),
-    update_uvs_1(Cs, We, Etab);
-update_uvs_1([], We, Etab) -> We#we{es=Etab}.
+    We = update_uvs_1(Vpos, VFace, Vmap, We0),
+    update_uvs(Cs, We);
+update_uvs([], We) -> We.
 
-update_uvs_2([{V0,{X,Y,_}}|Vs], [{V0,Fs}|VFs], Vmap, We, Etab0) ->
+update_uvs_1([{V0,{X,Y,_}}|Vs], [{V0,Fs}|VFs], Vmap, We0) ->
     UV = {X,Y},
     V = auv_segment:map_vertex(V0, Vmap),
-    Etab = foldl(fun(Face, A) ->
-			 update_uvs_3(V, Face, UV, A, We)
-		 end, Etab0, Fs),
-    update_uvs_2(Vs, VFs, Vmap, We, Etab);
-update_uvs_2([{V0,none}|Vs], [{V0,Fs}|VFs], Vmap, We, Etab0) ->
+    We = wings_va:set_vtx_face_uvs(V, Fs, UV, We0),
+    update_uvs_1(Vs, VFs, Vmap, We);
+update_uvs_1([{V0,none}|Vs], [{V0,Fs}|VFs], Vmap, We0) ->
     V = auv_segment:map_vertex(V0, Vmap),
-    Etab = foldl(fun(Face, A) ->
-			 update_uvs_3(V, Face, none, A, We)
-		 end, Etab0, Fs),
-    update_uvs_2(Vs, VFs, Vmap, We, Etab);
-update_uvs_2([], [], _, _, Etab) -> Etab.
-
-update_uvs_3(V, Face, UV, Etab, We) ->
-    wings_vertex:fold(
-      fun(Edge, _, Rec0, Et) ->
-	      case Rec0 of
-		  #edge{vs=V,lf=Face,a=A} when A =/= UV ->
-		      Rec = array:get(Edge, Et),
-		      array:set(Edge, Rec#edge{a=UV}, Et);
-		  #edge{ve=V,rf=Face,b=B} when B =/= UV ->
-		      Rec = array:get(Edge, Et),
-		      array:set(Edge, Rec#edge{b=UV}, Et);
-		  _ -> Et
-	      end
-      end, Etab, V, We).
+    We = wings_va:set_vtx_face_uvs(V, Fs, none, We0),
+    update_uvs_1(Vs, VFs, Vmap, We);
+update_uvs_1([], [], _, We) -> We.
 
 insert_material(Cs, MatName, We) ->
     Faces = lists:append([wings_we:visible(W) || W <- gb_trees:values(Cs)]),

commit f2e9cd53ed433a1130e9cb6855e09df578d99d52

Fix broken Tools|Snap Image
Commit ddad5076ed6a36b6989d57e9b73681a2cc2269d6 broke
Tools|Snap Image so that it would crash most of the time.

Instead of fixing only that problem, rewrite the updating
of UV coordinates to follow the same approach as in AutoUV,
which is faster and preserves vertex colors.

Also make sure that vertex colors (if any) do not override
the texture. If the vertex color attribute for the material
is 'Set', change it to 'Multiply' so that vertex colors and
the texture will be combined.

NOTE: Tools|Snap Image was broken in several ways. It will now
also preserve vertex colors. (Thanks to PuzzledPaul.) [bjorng]

plugins_src/autouv/wpc_snap.erl
@@ -216,17 +216,22 @@ draw_image(Image,_St) ->
     gl:'end'(),
     gl:popAttrib().
     
-calc_uv(_V = {X,Y,Z}) ->
-    {MM,PM,Viewport = {_,_,W,H}} = wings_u:get_matrices(0, original),
-    {S,T, _} = wings_gl:project(X,Y,Z,MM,PM,Viewport),
+calc_uv_fun() ->
+    %% First do all the view-dependent calculations that are
+    %% common for all vertices.
+    {MM,PM,{_,_,W,H}=Viewport} = wings_u:get_matrices(0, original),
     #s{w=IW,h=IH,sx=Sx,sy=Sy,tx=Tx,ty=Ty} = get(?MODULE),
-    {Xs,Ys} = scale(W,H,IW,IH),
-    Center = 0.5,
-    Res = {Tx/2+(S/W*Xs*Sx+Center-Sx*Xs/2),Ty/2+(T/H*Sy*Ys+Center-Sy*Ys/2)},
-%%    io:format("~p st ~.3f,~.3f XsYs ~.2f ~.2f Res ~p~n", [_V, S/W,T/H,Xs,Ys,Res]),
-    Res.
+    {Xs,Ys} = scale(W, H, IW, IH),
 
-scale(W,H,IW,IH) ->
+    %% In the fun, do the calculations that are specific
+    %% for each vertex.
+    fun({X,Y,Z}) ->
+	    {S,T,_} = wings_gl:project(X, Y, Z, MM, PM, Viewport),
+	    Center = 0.5,
+	    {Tx/2+(S/W*Xs*Sx+Center-Sx*Xs/2),Ty/2+(T/H*Sy*Ys+Center-Sy*Ys/2)}
+    end.
+
+scale(W, H, IW, IH) ->
     if 
 	W == H ->
 	    if 
@@ -255,27 +260,25 @@ scale(W,H,IW,IH) ->
  	    end
     end.
 
-update_uv_fun(Vtab) ->
-    fun(Face, V, Edge, Rec0, We) ->
-	    case Rec0 of
-		#edge{vs=V,lf=Face} ->
-		    Vpos = array:get(V, Vtab),
-		    UV = calc_uv(Vpos),
-		    Attr = wings_va:new_attr(uv, UV),
-		    wings_va:set_edge_attrs(Edge, left, Attr, We);
-		#edge{ve=V,rf=Face} ->
-		    Vpos = array:get(V, Vtab),
-		    UV = calc_uv(Vpos),
-		    Attr = wings_va:new_attr(uv, UV),
-		    wings_va:set_edge_attrs(Edge, right, Attr, We)
-	    end
-    end.
-
 insert_uvs(St0) ->
-    wings_sel:map(fun(Items, We=#we{vp=Vtab}) ->
-			  AddUv = update_uv_fun(Vtab),
-			  wings_face:fold_faces(AddUv, We, Items, We)
+    CalcUV = calc_uv_fun(),
+    wings_sel:map(fun(Faces, We) ->
+			  insert_we_uvs(Faces, CalcUV, We)
 		  end, St0).
+
+insert_we_uvs(Faces, CalcUV, We) ->
+    VFace = wings_face:fold_faces(
+	      fun(Face, V, _, _, A) ->
+		      [{V,Face}|A]
+	      end, [], Faces, We),
+    VFaces = wings_util:rel2fam(VFace),
+    insert_we_uvs_1(VFaces, CalcUV, We).
+    
+insert_we_uvs_1([{V,Faces}|T], CalcUV, We0) ->
+    UV = CalcUV(wings_vertex:pos(V, We0)),
+    We = wings_va:set_vtx_face_uvs(V, Faces, UV, We0),
+    insert_we_uvs_1(T, CalcUV, We);
+insert_we_uvs_1([], _, We) -> We.
 
 set_materials(Image,St0) ->     
     Fix = fun(Items,We0,NewMats0) ->
@@ -299,7 +302,7 @@ dup_mat(MatName,{Used,St0},{Image,Name}) ->
     Maps0 = proplists:get_value(maps, Mat0),
     case proplists:get_value(diffuse, Maps0) of
 	Image -> 
-	    %% It already has the texture no need to create new material
+	    %% It already has the texture; no need to create new material
 	    {MatName,{[{MatName,MatName}|Used],St0}};
 	Else ->
 	    NewMatName = list_to_atom(atom_to_list(MatName) ++ "_" ++ Name),
@@ -307,7 +310,21 @@ dup_mat(MatName,{Used,St0},{Image,Name}) ->
 		       undefined -> [{diffuse,Image}|Maps0];
 		       _ -> lists:keyreplace(diffuse,1,Maps0,{diffuse,Image})
 		   end,
-	    Mat   = {NewMatName,lists:keyreplace(maps,1,Mat0, {maps,Maps})},
+
+	    %% Make sure that vertex colors will not override the texture.
+	    %% If the Vertex Color attribute in the material is 'Set',
+	    %% change it to 'Multiply'.
+	    OpenGL0 = proplists:get_value(opengl, Mat0),
+	    VtxColors = case proplists:get_value(vertex_colors, OpenGL0) of
+			    set -> multiply;
+			    Other -> Other
+			end,
+	    OpenGL1 = proplists:delete(vertex_colors, OpenGL0),
+	    OpenGL = [{vertex_colors,VtxColors}|OpenGL1],
+
+	    Mat1 = proplists:delete(opengl, Mat0),
+	    Mat2 = proplists:delete(maps, Mat1),
+	    Mat = {NewMatName,[{opengl,OpenGL},{maps,Maps}|Mat2]},
 	    case wings_material:add_materials([Mat], St0) of
 		{St,[]} ->
 		    {NewMatName, {[{MatName,NewMatName}|Used],St}};