dgud/wings

commit 21c35cefd6473ae7c74fd5f3f6eae58526d77829

Fix saving/loading of area light geometry
Commit af2d517f9ee77a87f6f94b4ae82ee80d1ae2d542 changed the
definition of the e3d_face record, which broke compatibility
with .wings files containing area lights (since the geometry
for area lights are saved using the e3d_mesh and e3d_face
records).

Ideally, we should not use records at all for saving the
geometry of area lights, but changing the format requires
careful planning and implementation to preserve compatibility,
so we will not do that now.

Instead, change the saving of area lights so that they are
saved using the definition of e3d_face before 1.1.9. When
loading area lights, handle both the 1.1.9 e3d_face definition
and the classic definition. (We do all this by manipulating
records as tuples using element/2 and setelement/3.)

NOTE: Wings 1.1.9 (and 1.1.9.1) would crash when attempting to open
a .wings file containing area lights saved by a previous version of
Wings. This has been corrected.  Furthermore, when saving area
lights, they will be saved in the same way as in versions before
1.1.9. That means that a .wings file containing area lights saved
by this version of Wings cannot be opened by 1.1.9.x, but it can
be opened by older versions such as 1.0.1 and 1.1.8.
(Thanks to Micheus.) [bjorng]

src/wings_light.erl
@@ -611,7 +611,7 @@ get_light_1(#we{light=L}=We) ->
 		 true -> OpenGL0
 	     end,
     OpenGL = case Type of
-		 area -> [{mesh,wings_export:make_mesh(We, [])}|OpenGL1];
+		 area -> [{mesh,export_mesh(We)}|OpenGL1];
 		 _ -> OpenGL1
 	     end,
     [{opengl,OpenGL}|Prop].
@@ -622,6 +622,29 @@ export_perm({_,_}, Ps) ->
     [{visible,false},{locked,false}|Ps];
 export_perm(P, Ps) when is_integer(P) ->
     [{visible,P < 2},{locked,(P band 1) =/= 0}|Ps].
+
+%% This is the classic definition of e3d_face{}, as it was defined
+%% before 1.1.9.
+-record(classic_e3d_face,
+	{vs=[],				        %List of vertex indices.
+	 vc=[],					%Vertex color indices.
+	 tx=[],				        %List of texture indices.
+	 ns=[],				        %List of normal indices.
+	 mat=[],				%Materials for face.
+	 vis=-1}).				%Visible edges (as in 3DS).
+
+export_mesh(We) ->
+    #e3d_mesh{fs=Fs0} = Mesh = wings_export:make_mesh(We, []),
+    Fs = [export_fix_face(F) || F <- Fs0],
+    Mesh#e3d_mesh{fs=Fs}.
+
+export_fix_face(#e3d_face{vs=Vs,mat=Mat}) ->
+    %% Fix the face record so that it looks like the classic
+    %% definition of #e3d_face{} (before 1.1.9).
+    FaceRec = #classic_e3d_face{vs=Vs,mat=Mat},
+
+    %% Patch the record type.
+    setelement(1, FaceRec, e3d_face).
 
 %%%
 %%% Importing lights.
@@ -667,7 +690,7 @@ import_we(#light{type=area}=Light, OpenGL, {X,Y,Z}) ->
 					mat=['_hole_']}],
 			  vs=[{X+1.0,Y,Z+1.0},{X-1.0,Y,Z+1.0},
 			      {X-1.0,Y,Z-1.0},{X+1.0,Y,Z-1.0}]};
-	    {mesh,M} -> e3d_mesh:transform(e3d_mesh:clean_faces(M))
+	    {mesh,M} -> import_fix_mesh(M)
 	end,
     We = wings_import:import_mesh(material, Mesh),
     We#we{light=Light,has_shape=true};
@@ -683,6 +706,24 @@ import_we(#light{}=Light, _OpenGL, {X,Y,Z}) ->
 	  {X-S,Y-S,Z-S},{X-S,Y+S,Z-S},{X+S,Y+S,Z-S},{X+S,Y-S,Z-S}],
     We = wings_we:build(Fs, Vs),
     We#we{light=Light,has_shape=false}.
+
+import_fix_mesh(#e3d_mesh{fs=Fs0}=Mesh0) ->
+    Fs = [import_fix_face(F) || F <- Fs0],
+    Mesh1 = Mesh0#e3d_mesh{fs=Fs},
+    Mesh = e3d_mesh:clean_faces(Mesh1),
+    e3d_mesh:transform(Mesh).
+
+import_fix_face(FaceRec) when is_tuple(FaceRec) ->
+    %% Different versions of Wings can have #e3d_face{}
+    %% records of different size (for example, in Wings 1.1.9
+    %% a new 'sg' field was added to #e3d_face{}). We know
+    %% that the fields we are interested in are in the same
+    %% place, so we can retrieve them using element/2.
+    %%
+    e3d_face = element(1, FaceRec),		%Crash on unknown record type.
+    Vs = element(#e3d_face.vs, FaceRec),
+    Mat = element(#e3d_face.mat, FaceRec),
+    #e3d_face{vs=Vs,mat=Mat}.
 
 %%%
 %%% Setting up lights.

commit f883994416ed42d2c7e20e3d2dfde06cdbadf330

Fix crash in Yafray export of area lights
Commit 21c35cefd6473ae7c74fd5f3f6eae58526d77829 broke Yafray
export of area lights, since wings_light:export/1 will encode
area lights using the previous definition of #e3d_face{}.

Fix this problem by reverting wings_light:export/1 to exporting
area lights with the current definition of #e3d_face{}, and introduce
the wings_light:export_bc/1 function to be used for saving
area lights in a portable way in .wings files.

NOTE: Exporting a scene with area lights with the Yafray exporter
would cause a crash. (Thanks to spacemanspiff7.) [bjorng]

src/wings_ff_wings.erl
@@ -572,7 +572,7 @@ translate_old_holes_1(#we{fs=Ftab}=We0) ->
 export(Name, St0) ->
     wings_pb:start( ?__(1,"saving")),
     wings_pb:update(0.01, ?__(2,"lights")),
-    Lights = wings_light:export(St0),
+    Lights = wings_light:export_bc(St0),
     Materials = case wings_pref:get_value(save_unused_materials) of
         true -> 
             #st{mat=Mat} = St0,

src/wings_light.erl
@@ -15,7 +15,8 @@
 -export([light_types/0,menu/3,command/2,is_any_light_selected/1,info/1,
 	 create/2,update_dynamic/2,update_matrix/2,update/1,render/1,
 	 modeling_lights/2,global_lights/0,camera_lights/0,
-	 export/1,export_camera_lights/0,import/2,import/1,shape_materials/2,
+	 export/1,export_bc/1,export_camera_lights/0,
+	 import/2,import/1,shape_materials/2,
 	 light_pos/1]).
 
 -define(NEED_OPENGL, 1).
@@ -309,7 +310,7 @@ edit(_) -> wings_u:error(?__(1,"Select only one light.")).
 
 edit(Id, #st{shapes=Shs}=St) ->
     We = #we{light=#light{type=Type}} = gb_trees:get(Id, Shs),
-    {Name,Prop} = get_light(We),
+    {Name,Prop} = get_light(We, false),
     case Type of
 	ambient ->
 	    {dialog,Qs,Fun} = edit_ambient_dialog(Name, Prop, We, Shs, St),
@@ -558,9 +559,17 @@ render(#dlo{work=Light}) ->
 %%% Exporting lights.
 %%%
 
-export(#st{shapes=Shs}) ->
+%% For exporters.
+export(St) ->
+    export(St, false).
+
+%% For saving in .wings files.
+export_bc(St) ->
+    export(St, true).
+
+export(#st{shapes=Shs}, BackwardsCompatible) ->
     L = foldl(fun(We, A) when ?IS_ANY_LIGHT(We) ->
-		      [get_light(We)|A];
+		      [get_light(We, BackwardsCompatible)|A];
 		 (_, A) -> A
 	      end, [], gb_trees:values(Shs)),
     reverse(L).
@@ -581,20 +590,20 @@ export_camera_lights() ->
 		 We = #we{name = Name,
 			  vp = array:from_orddict([{1, LPos}]),
 			  light = Li#light{aim=Aim}},
-		 get_light(We)
+		 get_light(We, false)
 	 end,
     [GL(Light) || Light <- [Amb|Ls]].
 
-get_light(#we{name=Name,perm=P}=We) ->
-    Ps0 = get_light_1(We),
+get_light(#we{name=Name,perm=P}=We, BC) ->
+    Ps0 = get_light_1(We, BC),
     Ps = export_perm(P, Ps0),
     {Name,Ps}.
 
-get_light_1(#we{light=#light{type=ambient,ambient=Amb,prop=Prop}}=We) ->
+get_light_1(#we{light=#light{type=ambient,ambient=Amb,prop=Prop}}=We, _) ->
     P = light_pos(We),
     OpenGL = [{type,ambient},{ambient,Amb},{position,P}],
     [{opengl,OpenGL}|Prop];
-get_light_1(#we{light=L}=We) ->
+get_light_1(#we{light=L}=We, BC) ->
     #light{type=Type,diffuse=Diff,ambient=Amb,specular=Spec,
 	   aim=Aim,spot_angle=Angle,spot_exp=SpotExp,
 	   lin_att=LinAtt,quad_att=QuadAtt,prop=Prop} = L,
@@ -614,7 +623,7 @@ get_light_1(#we{light=L}=We) ->
 		 true -> OpenGL0
 	     end,
     OpenGL = case Type of
-		 area -> [{mesh,export_mesh(We)}|OpenGL1];
+		 area -> [{mesh,export_mesh(We, BC)}|OpenGL1];
 		 _ -> OpenGL1
 	     end,
     [{opengl,OpenGL}|Prop].
@@ -636,9 +645,14 @@ export_perm(P, Ps) when is_integer(P) ->
 	 mat=[],				%Materials for face.
 	 vis=-1}).				%Visible edges (as in 3DS).
 
-export_mesh(We) ->
+export_mesh(We, BC) ->
     #e3d_mesh{fs=Fs0} = Mesh = wings_export:make_mesh(We, []),
-    Fs = [export_fix_face(F) || F <- Fs0],
+    Fs = case BC of
+	     false ->
+		 Fs0;
+	     true ->
+		 [export_fix_face(F) || F <- Fs0]
+	 end,
     Mesh#e3d_mesh{fs=Fs}.
 
 export_fix_face(#e3d_face{vs=Vs,mat=Mat}) ->