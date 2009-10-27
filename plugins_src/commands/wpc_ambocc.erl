fb6f0e5c04df79d6cec0643753b4c9a014654dd7
Dan Gudmundsson <dgud@users.sourceforge.net>
2009-10-27 22:06:03 +0100

    Remove use of shaders
    I couldn't get shaders to work as good as expected.
    
    Anthony's original did better so I invoke his code, still uses
    fbos for cards that have that. It would probably be a good
    idea to combine the two modules into one module.
    
    NOTE: Optimized and improved the ambient occlusion plugin. [dgud]

--- plugins_src/commands/wpc_ambocc.erl
+++ plugins_src/commands/wpc_ambocc.erl
@@ -8,7 +8,7 @@
 %%     used to sample the environment's visibility. The results are stored in
 %%     the vertex-colors and can also be baked to a texture through autouv.
 %%
-%%  CopyRight (c) 2008-2009 Anthony D'Agostino
+%%  CopyRight (c) 2008-2009 Anthony D'Agostino, Dan Gudmundsson
 %%
 %%  See the file "license.terms" for information on usage and redistribution
 %%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
@@ -18,8 +18,9 @@
 %% Plugin entry points
 -export([init/0,menu/2,command/2]).
 
-%% Reused by ambocc_glsl.erl
--export([make_disp_list/1]).
+%% Reused by ambocc_gl2.erl
+-export([make_disp_list/1, get_up_right/1, render_hemicube/5,
+	 get_ao_factor/1]).
 
 
 -define(NEED_OPENGL, 1).
@@ -33,14 +34,14 @@ menu({tools}, Menu) ->
 			"Add Ambient-Occlusion vertex colors via OpenGL"}];
 menu(_, Menu) -> Menu.
 
-command({tools,ambient_occlusion}, St) ->
-    case wings_gl:have_fbo() of
-	true ->
-	     ambient_occlusion(St);
-	    %ambocc_gl2:ambient_occlusion(St);
-	false ->
-	    ambient_occlusion(St)
-    end;
+command({tools,ambient_occlusion}, St0) ->
+    St = case wings_gl:have_fbo() of
+	     true ->
+		 ambocc_gl2:ambient_occlusion(St0);
+	     _ ->
+		 ambient_occlusion(St0)
+	 end,
+    create_ambient_light(St);
 command(_Cmd, _) -> next.
 
 ambient_occlusion(St) ->
@@ -58,8 +59,7 @@ ambient_occlusion(St) ->
     Seconds = timer:now_diff(EndTime,StartTime)/1.0e6,
     VidCard = gl:getString(?GL_RENDERER),
     io:fwrite("OpenGL AmbOcc time: ~.1fs (~s)\n", [Seconds,VidCard]),
-    St3 = create_ambient_light(St2),
-    St3.
+    St2.
 
 process_obj(We, _) when ?IS_NOT_VISIBLE(We#we.perm) ->
     We;
@@ -73,17 +73,19 @@ process_obj(We, _) when ?IS_ANY_LIGHT(We) ->
 process_obj(We0, DispList) ->
     #we{es=Etab,vp=Vtab,name=Name} = We0,
     io:fwrite("Processing: ~s\n", [Name]),
-    GetColor = fun(Key,_Val) ->
-	Eye = wings_vertex:pos(Key,We0) ,
-	LookAt = wings_vertex:normal(Key,We0),
-	get_ao_color(Eye,LookAt,DispList)
-    end,
+    GetColor =
+	fun(Key,_Val) ->
+		Eye = wings_vertex:pos(Key,We0) ,
+		LookAt = wings_vertex:normal(Key,We0),
+		get_ao_color(Eye,LookAt,DispList)
+	end,
     VertexColors = array:sparse_map(GetColor, Vtab),
-    SetColor = fun(Edge, #edge{vs=Va,ve=Vb}, W) ->
-	Color1 = array:get(Va, VertexColors),
-	Color2 = array:get(Vb, VertexColors),
-        wings_va:set_edge_color(Edge, Color1, Color2, W)
-    end,
+    SetColor =
+	fun(Edge, #edge{vs=Va,ve=Vb}, W) ->
+		Color1 = array:get(Va, VertexColors),
+		Color2 = array:get(Vb, VertexColors),
+		wings_va:set_edge_color(Edge, Color1, Color2, W)
+	end,
     array:sparse_foldl(SetColor, We0, Etab).
 
 make_disp_list(St) ->
@@ -137,25 +139,23 @@ setup_gl() ->
 
 get_ao_color(Eye, Lookat, DispList) ->
     gl:clear(?GL_COLOR_BUFFER_BIT bor ?GL_DEPTH_BUFFER_BIT),
-    render_hemicube(Eye, Lookat, DispList),
+    render_hemicube(0, 0, Eye, Lookat, DispList),
     Factor = read_frame(),
     {Factor,Factor,Factor}.
 
-get_up_right({0.0,0.0,1.0}) ->
-    Up = {0.0,+1.0,0.0},
-    Right = {1.0,0.0,0.0},
-    {Up,Right};
-get_up_right({0.0,0.0,-1.0}) ->
-    Up = {0.0,-1.0,0.0},
-    Right = {1.0,0.0,0.0},
-    {Up,Right};
 get_up_right(Lookat) ->
-    Up1 = {0.0,0.0,1.0},
+    Up1   = up(Lookat),
     Right = e3d_vec:cross(Up1, Lookat),
-    Up = e3d_vec:cross(Lookat, Right),
+    Up    = e3d_vec:cross(Lookat, Right),
     {Up,Right}.
 
-render_hemicube(Eye, Lookat, DispList) ->
+up({X,Y,Z})
+  when abs(Y) > abs(X), abs(Y) > abs(Z) ->
+    {1.0,0.0,0.0};
+up(_) ->
+    {0.0,1.0,0.0}.
+
+render_hemicube(SX, SY, Eye, Lookat, DL) ->
     {Up,Right} = get_up_right(Lookat),
     Hemirez = 64, % Must be even and/or power-of-two
     P1 = trunc(Hemirez * 0.00),
@@ -169,11 +169,11 @@ render_hemicube(Eye, Lookat, DispList) ->
     EmU = e3d_vec:sub(Eye, Up),
     EpU = e3d_vec:add(Eye, Up),
     LookatN = e3d_vec:neg(Lookat),
-    render_view(Eye, EpL, Up,      [-1,1,-1,1], [P2,P2,W,W], DispList), % Center
-    render_view(Eye, EpR, Up,      [ 0,1,-1,1], [P1,P2,H,W], DispList), % Right
-    render_view(Eye, EmR, Up,      [-1,0,-1,1], [P4,P2,H,W], DispList), % Left
-    render_view(Eye, EmU, Lookat,  [-1,1, 0,1], [P2,P1,W,H], DispList), % Down
-    render_view(Eye, EpU, LookatN, [-1,1,-1,0], [P2,P4,W,H], DispList). % Up
+    render_view(Eye, EpL, Up,      [-1,1,-1,1], [SX+P2,SY+P2,W,W], DL), % Center
+    render_view(Eye, EpR, Up,      [ 0,1,-1,1], [SX+P1,SY+P2,H,W], DL), % Right
+    render_view(Eye, EmR, Up,      [-1,0,-1,1], [SX+P4,SY+P2,H,W], DL), % Left
+    render_view(Eye, EmU, Lookat,  [-1,1, 0,1], [SX+P2,SY+P1,W,H], DL), % Down
+    render_view(Eye, EpU, LookatN, [-1,1,-1,0], [SX+P2,SY+P4,W,H], DL). % Up
 
 render_view(Eye, Lookat, Up, Frustum, Viewport, DispList) ->
     Near = 0.01,
