0e64eba1de0214edcaa7abda6ea635e8f0c6f1e7
Dan Gudmundsson <dgud@users.sourceforge.net>
2014-03-19 22:03:29 +0100

    Fix new type specs requires OTP-17.0

--- src/wings_undo.erl
+++ src/wings_undo.erl
@@ -30,12 +30,12 @@
 	 
 %% The essential part of the state record.
 -record(est,
-	{shapes=[] :: list(#we{}) | gb_tree(),
+	{shapes=[] :: list(#we{}) | gb_trees:tree(),
 	 selmode=face :: sel_mode(),
 	 sel=[] :: list(),
 	 onext=1 :: elem_num(),
 	 mat=wings_material:default(),
-	 pst=gb_trees:empty() :: gb_tree(),
+	 pst=gb_trees:empty() :: gb_trees:tree(),
 
 	 %% For the Develop menu.
 	 cmd,
