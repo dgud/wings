28d9ac6a328da1fc95ee4429f595156b00977210
Dan Gudmundsson <dgud@users.sourceforge.net>
2014-08-22 08:07:33 +0200

    Remove need for SDL and esdl
    To simplify the building of all the platforms, we remove the need
    for esdl lib, we only use the header files anyway.

---
+++ src/sdl_mouse.hrl
@@ -0,0 +1,25 @@
+%%  Copyright (c) 2001 Dan Gudmundsson
+%%
+%%  See the file "license.terms" for information on usage and redistribution
+%%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+%% 
+%%     $Id$
+%%
+%%%----------------------------------------------------------------------
+%%% File    : sdl_mouse.hrl
+%%% Author  : Dan Gudmundsson <dgud@erix.ericsson.se>
+%%% Purpose : Usable mouse macros
+%%% Created : 12 Jul 2000 by Dan Gudmundsson <dgud@erix.ericsson.se>
+%%%----------------------------------------------------------------------
+
+-define(SDL_BUTTON_LEFT,	1).
+-define(SDL_BUTTON_MIDDLE,	2).
+-define(SDL_BUTTON_RIGHT,	3).
+-define(SDL_BUTTON_X1,		4).
+-define(SDL_BUTTON_X2,		5).
+-define(SDL_BUTTON_LMASK,       2#00001).
+-define(SDL_BUTTON_MMASK,       2#00010).
+-define(SDL_BUTTON_RMASK,       2#00100).
+-define(SDL_BUTTON_X1MASK,      2#01000).
+-define(SDL_BUTTON_X2MASK,      2#10000).
+
