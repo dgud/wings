32ca1e385154b58354b2336b5c17c49f3925f8fe
Dan Gudmundsson <dgud@users.sourceforge.net>
2015-01-09 16:58:35 +0100

    Update menu checkmarks
    So that they are set correctly in menu when updated from hotkeys or
    via toolbar.

--- src/wings_menu.erl
+++ src/wings_menu.erl
@@ -16,7 +16,8 @@
 -export([is_popup_event/1,menu/5,popup_menu/4,build_command/2,
 	 kill_menus/0]).
 -export([wx_menubar/1, id_to_name/1, check_item/1, str_clean/1]).
--export([update_menu/3, update_menu/4, update_menu_hotkey/2]).
+-export([update_menu/3, update_menu/4,
+	 update_menu_enabled/3, update_menu_hotkey/2]).
 
 -define(NEED_ESDL, 1).
 -include("wings.hrl").
@@ -442,7 +443,7 @@ check_item(Name) ->
 update_menu(Menu, Item, Cmd) ->
     update_menu(Menu, Item, Cmd, undefined).
 update_menu(Menu, Item, Cmd, Help) ->
-    Id = predefined_item(Menu, Item),
+    Id = menu_item_id(Menu, Item),
     MI = case ets:lookup(wings_menus, Id) of
 	     [#menu_entry{object=MO}] ->
 		 MO;
@@ -460,6 +461,11 @@ update_menu(Menu, Item, Cmd, Help) ->
     wxMenuItem:setText(MI, Cmd),
     is_list(Help) andalso wxMenuItem:setHelp(MI, Help).
 
+update_menu_enabled(Menu, Item, Enabled)
+  when is_boolean(Enabled) ->
+    Id = menu_item_id(Menu, Item),
+    [#menu_entry{object=MI}] = ets:lookup(wings_menus, Id),
+    wxMenuItem:isCheckable(MI) andalso wxMenuItem:check(MI, [{check,Enabled}]).
 
 update_menu_hotkey(Action, HotKeyStr) ->
     case ets:match_object(wings_menus, #menu_entry{name=Action, _='_'}) of
@@ -471,6 +477,19 @@ update_menu_hotkey(Action, HotKeyStr) ->
 		_ -> wxMenuItem:setText(MI, Label ++ "\t" ++ HotKeyStr)
 	    end
     end.
+
+menu_item_id(Menu, Item) ->
+    case predefined_item(Menu, Item) of
+	Id when is_integer(Id) ->
+	    Id;
+	false ->
+	    case ets:match_object(wings_menus, #menu_entry{name={Menu,Item}, _='_'}) of
+		[#menu_entry{wxid=Id}] -> Id;
+		[] -> false
+	    end
+    end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 wx_menubar(Menus) ->
     ets:new(wings_menus, [named_table, {keypos,2}]),
