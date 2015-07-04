b08342822ff3391cba72f020bd968c97b8be3c40
Dan Gudmundsson <dgud@users.sourceforge.net>
2015-07-04 10:53:38 +0200

    Remove #st{} from top level menus
    We can't handle it anyway, it must be explictly updated when
    changed.

--- plugins_src/autouv/wpc_autouv.erl
+++ plugins_src/autouv/wpc_autouv.erl
@@ -16,8 +16,8 @@
 -define(NEED_OPENGL, 1).
 -define(NEED_ESDL, 1).
 
--include("wings.hrl").
--include("e3d_image.hrl").
+-include_lib("wings/src/wings.hrl").
+-include_lib("wings/e3d/e3d_image.hrl").
 -include("auv.hrl").
  
 -export([init/0,menu/2,command/2,redraw/1]).
@@ -273,8 +273,8 @@ menubar() ->
 	      [ShwBgImg|redundant_separators(Menu)]
       end},
      {?__(11,"Select"),select,
-      fun(St) ->
-	      Menu0 = wings_sel_cmd:menu(St),
+      fun(_St) ->
+	      Menu0 = wings_sel_cmd:menu(),
 	      Menu = [I || I <- Menu0,
 			   keep_sel_item(I)],
 	      redundant_separators(Menu)
