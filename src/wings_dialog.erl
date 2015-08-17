dgud/wings

commit 5f8c96f9d9ee76014de9ef632e30d77b9f2df069

Use native color selector for color chooser
Allow user to specify which color chooser he wants to use.

And refactor color conversion routines and palette handling in native
color_dialog.

Add dialog_parent handling so that dialog have the correct z order
otherwise dialogs end up behind another dialog, when invoking dialog
from dialog.

src/wings_dialog.erl
@@ -26,14 +26,17 @@
 	 get_value/2, set_value/3,
 	 get_widget/2]).
 
+%% Internal dialogs
+-export([return_result/3,
+	 get_dialog_parent/0, set_dialog_parent/1, reset_dialog_parent/1]).
+
 %%-compile(export_all).
 
 -record(in, {key, type, def, wx, wx_ext=[], validator, data, hook, output=true}).
--record(eh, {fs, apply, owner, type, pid}).
+-record(eh, {fs, apply, prev_parent, owner, type, pid}).
 
 %%
 %% Syntax of Qs.
-%% See also wpc_test_ask.erl for examples.
 %%
 %% Common types:
 %%     String = string()
@@ -41,61 +44,15 @@
 %%     Fields = [Field]
 %%     Field  -- Any field tuple described below
 %%     Key = term()
-%%         Note: An integer() Key is a relative field address,
-%%             any other term is an absolute storage identifier.
-%%             Since an integer() Key mean something it is a bad idea
-%%             to use a float() as Key. It should work, but the slightest
-%%             programming mistake in this module may lead to a mismatch.
-%%               Field common data (common for all field types, used for
-%%             dialog tree traversing, etc) is stored in the dialog tree,
-%%             consisting of nested #fi{} records.
-%%               Field private data (special to a given field type), and
-%%             field values are all stored in the same gb_trees store.
-%%               All fields have a field index in the dialog tree, which
-%%             is an integer from 1 and up assigned in tree traversal
-%%             order (depth first - same as Qs order).
-%%               Field private data is stored with negative field index as
-%%             key in the gb_trees store and is different record types
-%%             depending on field type. Field values is stored in the
-%%             gb_trees store with the field index as key (the default),
-%%             or if using an integer key, the field shares value with
-%%             another field and the key is a relative field index
-%%             offset. If the key is any other term it is the actual
-%%             key used in the store.
-%%               All fields that have a non-integer key will return
-%%             {Key,Value} as the field result of the dialog. The
-%%             others just returns Value. Most fields return values,
-%%             except when noted below.
 %%     Hook = function()
-%%         Hook should have arity 2 and return 'void' for any
-%%         unknown arguments, like this:
-%%         fun (is_disabled, {Var,I,Store}) -> Bool;
-%%                 %% Called at redraw time for all fields.
-%%                 %% For container fields the contained fields are
-%%                 %% disabled but not the container field.
-%%             (is_minimized, {Var,I,Store}) -> Bool;
-%%                 %% Called at layout time for all fields
-%%                 %% except Overlay frames.
-%%             (update, {Var,I,Val,Store}) ->
-%%                 keep|{store,NewStore}|done|{done,NewStore}|{layout,NewStore}
-%%                 %% Should make sure Val gets stored in NewStore.
-%%                 %% done|{done,_} finishes the dialog (may get restarted).
-%%                 %% {layout,_} forces a layout pass.
-%%                 %% Called when a field changes value.
-%%             (menu_disabled, {Var,I,Store}) -> [Disabled];
-%%                 %% Disabled = Val|{Val,MenuFlags}
-%%                 %% Val = menu value
-%%                 %% MenuFlags = menu flags prepended to other menu flags
-%%                 %% Only called for menu fields.
-%%             (_, _) -> void
+%%         Hook should have arity 3 and is invoked when something changes:
+%%         fun (Key, Value, Fields) -> ok;
 %%         end
 %%
 %% Qs is either one field below (preferably containing more fields,
 %% or a list of fields, in which case the list is enclosed in a
 %% {hframe,...} containing two {vframe,...}s, the first containing
 %% Qs and the second containing an Ok and a Cancel button.
-%%
-%%
 %%
 %% Container fields (dialog tree nodes):
 %%
@@ -370,6 +327,10 @@ set_value_impl(#in{wx=Ctrl, type=text}, Val, _) ->
     wxTextCtrl:changeValue(Ctrl, to_str(Val));
 set_value_impl(#in{wx=Ctrl, type=slider, data={_, ToSlider}}, Val, _) ->
     wxSlider:setValue(Ctrl, ToSlider(Val));
+set_value_impl(#in{wx=Ctrl, type=col_slider}, Val, _) ->
+    ww_color_slider:setColor(Ctrl, Val);
+set_value_impl(#in{wx=Ctrl, type=color}, Val, _) ->
+    ww_color_ctrl:setColor(Ctrl, Val);
 set_value_impl(In=#in{type=button}, Val, Store) ->
     true = ets:insert(Store, In#in{data=Val});
 set_value_impl(In=#in{type=value}, Val, Store) ->
@@ -403,19 +364,41 @@ queries(Qs0) ->
     Rows = to_label_column(Qs0),
     [{label_column, Rows}].
 
+%% ----------------------------------
+%% We can only have one active dialog at the time but
+%% they can open other dialog, i.e. color chooser from
+%% preferences
+
+get_dialog_parent() ->
+    case ?GET(dialog_parent) of
+	undefined -> ?GET(top_frame);
+	Tuple when element(1, Tuple) =:= wx_ref ->
+	    Tuple
+    end.
+
+set_dialog_parent(Dialog) ->
+    ?SET(dialog_parent, Dialog).
+
+reset_dialog_parent(Dialog) ->
+    Parent = wxWindow:getParent(Dialog),
+    ?SET(dialog_parent, Parent).
+
 enter_dialog(false, _, _, Fields, Fun) -> % No dialog return def values
     Values = get_output(default, Fields),
     true = ets:delete(element(1, Fields)),
     return_result(Fun, Values, wings_wm:this());
 enter_dialog(true, no_preview, Dialog, Fields, Fun) -> %% No preview cmd / modal dialog
+    set_dialog_parent(Dialog),
     case wxDialog:showModal(Dialog) of
 	?wxID_CANCEL ->
 	    true = ets:delete(element(1, Fields)),
+	    reset_dialog_parent(Dialog),
 	    wxDialog:destroy(Dialog),
 	    keep;
 	Result ->
 	    Values = get_output(Result, Fields),
 	    true = ets:delete(element(1, Fields)),
+	    reset_dialog_parent(Dialog),
 	    wxDialog:destroy(Dialog),
 	    return_result(Fun, Values, wings_wm:this())
     end;
@@ -432,11 +415,13 @@ enter_dialog(true, PreviewType, Dialog, Fields, Fun) ->
 					      [{id, ?wxID_OK},
 					       {lastId, ?wxID_NO},
 					       {callback,Forward}]),
+			     set_dialog_parent(Dialog),
 			     wxDialog:show(Dialog),
 			     wings_wm:psend(send_after_redraw, dialog_blanket, preview),
 			     receive
 				 closed ->
 				     true = ets:delete(element(1, Fields)),
+				     reset_dialog_parent(Dialog),
 				     wxDialog:destroy(Dialog)
 			     end
 		     end),
@@ -526,8 +511,8 @@ get_curr_value(#in{type=color, wx=Ctrl}) ->
     ww_color_ctrl:getColor(Ctrl);
 get_curr_value(#in{type=slider, wx=Ctrl, data={Convert,_}}) ->
     Convert(wxSlider:getValue(Ctrl));
-%% get_curr_value(In=#in{type=col_slider, wx=Ctrl}) ->
-%%     ww_color_slider:getColor(Ctrl);
+get_curr_value(#in{type=col_slider, wx=Ctrl}) ->
+    ww_color_slider:getColor(Ctrl);
 get_curr_value(#in{type=choice, wx=Ctrl}) ->
     wxChoice:getClientData(Ctrl,wxChoice:getSelection(Ctrl));
 get_curr_value(#in{type=text, def=Def, wx=Ctrl, validator=Validate}) ->
@@ -607,6 +592,13 @@ setup_hook(#in{key=Key, wx=Ctrl, type=color, hook=UserHook}, Fields) ->
 					      UserHook(Key, Col, Fields)
 				      end}]),
     ok;
+setup_hook(#in{key=Key, wx=Ctrl, type=col_slider, hook=UserHook}, Fields) ->
+    ww_color_slider:connect(Ctrl, col_changed,
+			    [{callback, fun({col_changed, Col}) ->
+						UserHook(Key, Col, Fields)
+					end}]),
+    ok;
+
 setup_hook({Key, #in{wx=Ctrl, type=fontpicker, hook=UserHook}}, Fields) ->
     wxFontPickerCtrl:connect(Ctrl, command_fontpicker_changed,
 			     [{callback, fun(_, Obj) ->
@@ -662,7 +654,6 @@ return_result(Fun, Values, Owner) ->
     end,
     keep.
 
-
 to_label_column(Qs) ->
     lists:map(fun label_col/1, Qs).
 label_col({_Label,{menu,_,_}}=Entry) -> Entry;
@@ -675,7 +666,7 @@ build_dialog(false, _Title, Qs) ->
     {undefined, DialogData};
 build_dialog(AskType, Title, Qs) ->
     wx:batch(fun() ->
-		     Parent = ?GET(top_frame),
+		     Parent = get_dialog_parent(),
 		     Style0 = case os:type() of
 				  {unix, darwin} -> ?wxSTAY_ON_TOP;
 				  _ -> ?wxFRAME_FLOAT_ON_PARENT
@@ -801,7 +792,17 @@ build(Ask, {label, Label, Flags}, Parent, Sizer, In)
 			end, [], Lines0),
     Text = wxStaticText:new(Parent, ?wxID_ANY, Lines),
     add_sizer(label, Sizer, Text),
-    wxSizer:setItemMinSize(Sizer, Text, proplists:get_value(min_wsz, Flags, -1), -1),
+    MinSize = case proplists:get_value(min_wsz, Flags, -1) of
+		  Sz when Sz > 0 -> Sz;
+		  Sz ->
+		      case proplists:get_value(width, Flags) of
+			  undefined -> Sz;
+			  Chars ->
+			      {W, _, _, _} = wxWindow:getTextExtent(Parent, "W"),
+			      Chars*W
+		      end
+	      end,
+    wxSizer:setItemMinSize(Sizer, Text, MinSize, -1),
     In;
 build(Ask, {label_column, Rows}, Parent, Sizer, In) ->
     build(Ask, {label_column, Rows, []}, Parent, Sizer, In);
@@ -809,9 +810,8 @@ build(Ask, {label_column, Rows, Flags}, Parent, Sizer, In) ->
     MinSize =
 	if Ask =:= false -> -1;
 	   true ->
-		ST = wxStaticText:new(Parent, ?wxID_ANY, ""),
 		lists:foldl(fun({Str = [_|_],_}, Max) ->
-				    {W, _, _, _} = wxWindow:getTextExtent(ST, Str),
+				    {W, _, _, _} = wxWindow:getTextExtent(Parent, Str),
 				    max(W+5, Max);
 			       (separator, Max)  ->
 				    Max
@@ -890,28 +890,50 @@ build(Ask, {slider, {color, Def, Flags}}, Parent, Sizer, In) ->
 
 build(Ask, {slider, Flags}, Parent, Sizer, In) ->
     Def = proplists:get_value(value, Flags),
-    Range = proplists:get_value(range, Flags),
     false = undefined == Def,
-    false = undefined == Range,
-    {Min, Value, Max, Style, ToText, ToSlider} = slider_style(Def, Range),
-    Create = fun() ->
-		     Ctrl = wxSlider:new(Parent, ?wxID_ANY, Value, Min, Max, [{style, Style}]),
-		     tooltip(Ctrl, Flags),
-		     add_sizer(slider, Sizer, Ctrl),
-		     Ctrl
-	     end,
-    [#in{key=proplists:get_value(key,Flags), def=Def, data={ToText, ToSlider},
+    case proplists:get_value(color, Flags, false) of
+	false ->
+	    Type = slider,
+	    Range = proplists:get_value(range, Flags),
+	    false = undefined == Range,
+	    {Min, Value, Max, Style, ToText, ToSlider} = slider_style(Def, Range),
+	    Data = {ToText, ToSlider},
+	    Create = fun() ->
+			     Ctrl = wxSlider:new(Parent, ?wxID_ANY, Value, Min, Max, [{style, Style}]),
+			     tooltip(Ctrl, Flags),
+			     add_sizer(slider, Sizer, Ctrl),
+			     Ctrl
+		     end;
+	Color ->
+	    true = lists:member(Color, [rgb, red, green, blue, hue, sat, val]), %% Assert
+	    Type = col_slider,
+	    Data = undefined,
+	    Create = fun() ->
+			     Ctrl = ww_color_slider:new(Parent, ?wxID_ANY, Def, [{color, Color}]),
+			     tooltip(Ctrl, Flags),
+			     add_sizer(slider, Sizer, Ctrl),
+			     Ctrl
+		     end
+    end,
+    [#in{key=proplists:get_value(key,Flags), def=Def, data=Data,
 	 hook=proplists:get_value(hook, Flags),
-	 type=slider, wx=create(Ask,Create)}|In];
+	 type=Type, wx=create(Ask,Create)}|In];
 
 build(Ask, {color, Def, Flags}, Parent, Sizer, In) ->
     Create = fun() ->
-		     Ctrl = ww_color_ctrl:new(Parent, ?wxID_ANY, [{col, Def}]),
+		     Static = proplists:get_value(static, Flags, false),
+		     Dialog = proplists:get_value(native_dialog, Flags,
+						  wings_pref:get_value(color_dialog_native)),
+		     Ctrl = ww_color_ctrl:new(Parent, ?wxID_ANY,
+					      [{col, Def},
+					       {static, Static},
+					       {native_dialog, Dialog}]),
 		     tooltip(Ctrl, Flags),
 		     add_sizer(button, Sizer, Ctrl),
 		     Ctrl
 	     end,
     [#in{key=proplists:get_value(key,Flags), def=Def,
+	 hook=proplists:get_value(hook, Flags),
 	 type=color, wx=create(Ask,Create)}|In];
 
 build(Ask, {button, {text, Def, Flags}}, Parent, Sizer, In) ->
@@ -1130,7 +1152,7 @@ build_box(Ask, Type, Qs, Flags, Parent, Top, In0) ->
 	    Input = lists:foldl(fun(Q, Input) ->
 					build(Ask, Q, Parent, Sizer, Input)
 				end, In0, Qs),
-	    add_sizer(Type0, Top, Sizer),
+	    add_sizer(Type0, Top, Sizer, Flags),
 	    Input;
 	_ ->
 	    Ctrl = wxPanel:new(Parent, [{style, ?wxNO_BORDER}]),
@@ -1314,9 +1336,17 @@ slider_style(Def, {Min, Max})
     {0, ToSlider(Def), 100, ?wxSL_HORIZONTAL, ToText, ToSlider}.
 
 add_sizer(What, Sizer, Ctrl) ->
-    {Proportion, Border, Flags} = sizer_flags(What, wxBoxSizer:getOrientation(Sizer)),
+    add_sizer(What, Sizer, Ctrl, []).
+
+add_sizer(What, Sizer, Ctrl, Opts) ->
+    {Proportion0, Border0, Flags0} =
+	sizer_flags(What, wxBoxSizer:getOrientation(Sizer)),
     %% io:format("What ~p ~p => ~p ~n",[What, wxBoxSizer:getOrientation(Sizer), {Proportion, Border, Flags}]),
-    wxSizer:add(Sizer, Ctrl, [{proportion, Proportion}, {border, Border}, {flag, Flags}]).
+    Proportion = proplists:get_value(proportion, Opts, Proportion0),
+    Border = proplists:get_value(border, Opts, Border0),
+    Flags  = proplists:get_value(flag, Opts, Flags0),
+    wxSizer:add(Sizer, Ctrl,
+		[{proportion, Proportion},{border, Border},{flag, Flags}]).
 
 sizer_flags(label, ?wxHORIZONTAL)     -> {0, 0, ?wxALIGN_CENTER_VERTICAL};
 sizer_flags(label, ?wxVERTICAL)       -> {1, 0, ?wxALIGN_CENTER_VERTICAL};

src/wings_color.erl
@@ -12,9 +12,12 @@
 %%
 
 -module(wings_color).
--export([init/0,choose/1,choose/2,
+-export([init/0,choose/1,choose/2,choose/3,
 	 share/1,store/1,average/1,average/2,mix/3,white/0,
-	 rgb_to_hsv/3,hsv_to_rgb/3]).
+	 rgb_to_hsv/3,hsv_to_rgb/3,
+	 rgb3bv/1, rgb4bv/1, rgb3fv/1, rgb4fv/1,
+	 def_palette/0
+	]).
 
 -include("wings.hrl").
 -import(lists, [foreach/2,keyfind/3]).
@@ -26,22 +29,35 @@ init() ->
     Black = ?BLACK,
     White = ?WHITE,
     Standard = [Black,White,average([Black,White])],
-    put(wings_color_chosen, Black),
+    ?SET(wings_color_chosen, Black),
+    wings_pref:set_default(color_dialog_native, true),
+    wings_pref:set_default(color_ctrl_palette,
+			   [begin G = float(G0), {G,G,G} end
+			    || G0 <- lists:seq(0, 255, 255 div 15)]),
     foreach(fun(C0) ->
 		    C = wings_util:share(C0),
 		    put(C, C)
 	    end, Standard).
 
-choose(Done) ->
-    Color0 = get(wings_color_chosen),
-    choose_1(Color0,
-	     fun(Color) ->
-		     put(wings_color_chosen, Color),
-		     Done(Color)
-	     end).
+def_palette() ->
+    {fun() -> wings_pref:get_value(color_ctrl_palette) end,
+     fun(Palette) when is_list(Palette) ->
+	     wings_pref:set_value(color_ctrl_palette, Palette)
+     end}.
+
+choose(Done0) ->
+    Color0 = ?GET(wings_color_chosen),
+    Done = fun(Color) ->
+		   ?SET(wings_color_chosen, Color),
+		   Done0(Color)
+	   end,
+    choose(Color0, Done).
 
 choose(Color, Done) ->
-    choose_1(Color, Done).
+    choose(Color, Done, wings_pref:get_value(color_dialog_native)).
+
+choose(Color, Done, UseNative) ->
+    choose_1(Color, Done, UseNative).
 
 share({Same,Same}) -> {Same,Same};
 share({_,_}=UV) -> UV;
@@ -125,17 +141,32 @@ average_uvs_1([], A0, A1, L0) ->
     L = float(L0),
     {A0/L,A1/L}.
 
+
+rgb3bv({R,G,B}) -> {round(R*255),round(G*255),round(B*255)};
+rgb3bv({R,G,B,_}) -> {round(R*255),round(G*255),round(B*255)}.
+
+rgb4bv({R,G,B}) -> {round(R*255),round(G*255), round(B*255), 255};
+rgb4bv({R,G,B,A}) -> {round(R*255),round(G*255),round(B*255), round(A*255)}.
+
+rgb3fv({R,G,B}) -> {R/255,G/255,B/255};
+rgb3fv({R,G,B,_}) -> {R/255,G/255,B/255}.
+
+rgb4fv({R,G,B}) -> {R/255,G/255,B/255, 1.0};
+rgb4fv({R,G,B,A}) -> {R/255,G/255,B/255,A/255}.
+
+rgb_to_hsv({R,G,B}) ->
+    rgb_to_hsv(R,G,B).
 rgb_to_hsv(R, G, B) ->
     case internal_rgb_to_hsv(R, G, B) of
 	{undefined,S,V} -> {0.0,S,V};
 	HSV -> HSV
     end.
 
-rgb_to_hsv(R, G, B, OldHue) ->
-    case internal_rgb_to_hsv(R, G, B) of
-	{undefined,S,V} -> {OldHue,S,V};
-	HSV -> HSV
-    end.
+%% rgb_to_hsv(R, G, B, OldHue) ->
+%%     case internal_rgb_to_hsv(R, G, B) of
+%% 	{undefined,S,V} -> {OldHue,S,V};
+%% 	HSV -> HSV
+%%     end.
 
 internal_rgb_to_hsv(R, G, B) ->
     Max = lists:max([R,G,B]),
@@ -153,9 +184,11 @@ internal_rgb_to_hsv(R, G, B) ->
 		end,
     {Hue,Sat,V}.
 
+hsv_to_rgb({H,S,V}) ->
+    hsv_to_rgb(H,S,V).
 hsv_to_rgb(H, S, V) ->
     Min = V*(1-S),
-    if 
+    if
 	V < 1.0E-5 ->
 	    {0.0,0.0,0.0};
 	H =< 120.0 ->
@@ -179,10 +212,27 @@ convert_hsv(H,V,Min) ->
 %%% Local functions for color choser.
 %%%
 
--define(COL_PREVIEW_WIDTH, 60).
--define(COL_PREVIEW_HEIGHT, 100).
+choose_1(RGB0, Done, true) ->
+    CData = wxColourData:new(),
+    wxColourData:setColour(CData, rgb3bv(RGB0)),
+    wxColourData:setChooseFull(CData, true),
+    {GetPalette,SetPalette} = def_palette(),
+    set_palette(GetPalette, CData),
+    Dlg = wxColourDialog:new(wings_dialog:get_dialog_parent(), [{data, CData}]),
+    wxColourData:destroy(CData),
+    case wxDialog:showModal(Dlg) of
+	?wxID_CANCEL ->
+	    wxColourDialog:destroy(Dlg),
+	    keep;
+	?wxID_OK ->
+	    NewData = wxColourDialog:getColourData(Dlg),
+	    SetPalette(get_palette(NewData)),
+	    RGBCol = wxColourData:getColour(NewData),
+	    wxColourDialog:destroy(Dlg),
+	    wings_dialog:return_result(Done, rgb3fv(RGBCol), wings_wm:this())
+    end;
 
-choose_1(RGB0, Done) ->
+choose_1(RGB0, Done, false) ->
     {R1,G1,B1,A1} =
 	case RGB0 of
 	    {R0,G0,B0}    -> {R0,G0,B0,none};
@@ -192,24 +242,6 @@ choose_1(RGB0, Done) ->
     HRange   = {0.0,360.0},
     SIRange  = {0.0,1.0},
     {H1,S1,V1} = rgb_to_hsv(R1, G1, B1),
-    PaneColor = wings_pref:get_value(dialog_color),
-    Draw = fun(X, Y, W, _, Sto) ->
-		   H = ?COL_PREVIEW_HEIGHT,
-		   R = gb_trees:get(red, Sto),
-		   G = gb_trees:get(green, Sto),
-		   B = gb_trees:get(blue, Sto),
-		   Half = H div 2,
-		   wings_io:blend(PaneColor,
-				  fun(Col) ->
-					  wings_io:sunken_rect(X, Y, W, H, 
-							       {R,G,B}, 
-							       Col)
-				  end),
-		   gl:color3f(R1, G1, B1),
-		   gl:rectf(X+0.5, Y+Half, X+W, Y+H),
-		   gl:color3b(0, 0, 0),
-		   keep
-	   end,
     Aslider = case A1 of
 		  none -> [];
 		  _ ->
@@ -218,55 +250,67 @@ choose_1(RGB0, Done) ->
 				{slider,
 				 {text,A1,[{key,alpha},{range,RGBRange}]}}]}]
 	      end,
+    CC = [red,green,blue, hue,sat,val],
+    Hook = fun({Type, Key}, Value, Fields) ->
+		   RGB = case Type of
+			     slider -> Value;
+			     text ->
+				 OLD = wings_dialog:get_value({slider, color}, Fields),
+				 update_element(Key, Value, OLD)
+			 end,
+		   [wings_dialog:set_value({slider, K}, RGB, Fields)
+		    || K <- [color|CC], K =/= Key orelse Type =/= slider ],
+		   [wings_dialog:set_value({text, K}, get_element(K, RGB), Fields)
+		    || K <- CC, K =/= Key orelse Type =/= text],
+		   ok
+	   end,
+    TextOpt = fun(Key, Range) ->
+		      [{key,{text,Key}},
+		       {range, Range},
+		       {hook, Hook},
+		       {width, 6}]
+	      end,
+
+    SliderOpt = fun(Col) ->
+			[{value, {R0,G0,B0}},
+			 {color, Col}, {key, {slider, Col}},
+			 {hook, Hook}]
+		end,
+    W = [{width,2}],
     Qs = [{hframe,
-	   [{custom,?COL_PREVIEW_WIDTH,?COL_PREVIEW_HEIGHT,Draw},
-	    {eyepicker,fun eyepicker_update/2},
-	    {vframe,
-	     [{hframe,
-	       [{vframe,
-		 [{hframe,
-		   [{label,"R"},
-		    {text,R1,color_text_flags(r, {red,green,blue},
-					      {hue,sat,val}, RGBRange)}]},
-		  {hframe,
-		   [{label,"G"},
-		    {text,G1,color_text_flags(g, {green,red,blue},
-					      {hue,sat,val}, RGBRange)}]},
-		  {hframe,
-		   [{label,"B"},
-		    {text,B1,color_text_flags(b, {blue,red,green},
-					      {hue,sat,val}, RGBRange)}]}]},
-		{vframe,
-		 [{slider,color_slider_flags(r, {red,green,blue},
-					     {hue,sat,val}, RGBRange)},
-		  {slider,color_slider_flags(g, {green,red,blue},
-					     {hue,sat,val}, RGBRange)},
-		  {slider,color_slider_flags(b, {blue,red,green},
-					     {hue,sat,val}, RGBRange)}]}]},
-	      separator,
-	      {hframe,
-	       [{vframe,
-		 [{hframe,
-		   [{label,"H"},
-		    {text,H1,color_text_flags(h, {hue,sat,val},
-					      {red,green,blue}, HRange)}]},
-		  {hframe,
-		   [{label,"S"},
-		    {text,S1,color_text_flags(s, {sat,hue,val},
-					      {red,green,blue}, SIRange)}]},
-		  {hframe,
-		   [{label,"V"},
-		    {text,V1,color_text_flags(v, {val,hue,sat},
-					      {red,green,blue}, SIRange)}]}]},
-		{vframe,
-		 [{slider,color_slider_flags(h, {hue,sat,val},
-					     {red,green,blue}, HRange)},
-		  {slider,color_slider_flags(s, {sat,hue,val},
-					     {red,green,blue}, SIRange)},
-		  {slider,color_slider_flags(v, {val,hue,sat},
-					     {red,green,blue}, SIRange)}]}]}
-	      |Aslider]}]}],
-    Fun = fun([{red,R},{green,G},{blue,B}|More]) ->
+	   [{vframe,[panel,
+		     {color, {R0,G0,B0}, [{key, {slider, color}},
+					  {hook, Hook},
+					  {native_dialog, true}]},
+		     panel,
+		     {color, {R0,G0,B0}, [{static, true}]}]},
+	  {vframe,
+	   [{hframe,
+	     [{label,"R:", W}, {text,R1,TextOpt(red, RGBRange)},
+	      {slider, SliderOpt(red)}]},
+	    {hframe,
+	     [{label,"G:", W}, {text,G1,TextOpt(green, RGBRange)},
+	      {slider, SliderOpt(green)}
+	     ]},
+	    {hframe,
+	     [{label,"B:", W}, {text,B1,TextOpt(blue, RGBRange)},
+	      {slider, SliderOpt(blue)}
+	     ]},
+	    separator,
+	    {hframe,
+	     [{label,"H:", W}, {text,H1,TextOpt(hue, HRange)},
+	      {slider, SliderOpt(hue)}]},
+	    {hframe,
+	     [{label,"S:", W}, {text,S1,TextOpt(sat, SIRange)},
+	      {slider, SliderOpt(sat)}
+	     ]},
+	    {hframe,
+	     [{label,"V:", W},{text,V1,TextOpt(val, SIRange)},
+	      {slider, SliderOpt(val)}
+	     ]}
+	    |Aslider], [{proportion, 1}]}]}],
+
+    Fun = fun([{{slider,color},{R,G,B}}|More]) ->
 		  RGB = case keyfind(alpha, 1, More) of
 			    false -> {R,G,B};
 			    {alpha,A} -> {R,G,B,A}
@@ -274,50 +318,53 @@ choose_1(RGB0, Done) ->
 		  Done(RGB)
 	  end,
 
-wings_ask:dialog(?STR(choose_1,1,"Choose Color"), Qs, Fun).
+    wings_dialog:dialog(?STR(choose_1,1,"Choose Color"), Qs, Fun).
 
-color_slider_flags(T, {_K1,K2,K3}=K123, Kabc, Range) ->
-    [{color,{T,K2,K3}}|color_text_flags(T, K123, Kabc, Range)].
+element_no(red)   -> {rgb, 1};
+element_no(green) -> {rgb, 2};
+element_no(blue)  -> {rgb, 3};
+element_no(hue)   -> {hsv, 1};
+element_no(sat)   -> {hsv, 2};
+element_no(val)   -> {hsv, 3}.
 
-color_text_flags(T, {K1,K2,K3}, Kabc, Range) ->
-    [{key,K1},{range,Range},{width,5},{hook,color_update(T, {K2,K3}, Kabc)}].
-
-color_update(T, {K1,K2}, {Ka,Kb,Kc}) ->
-    fun (update, {Var,_I,Val,Store0}) ->
-	    V1 = gb_trees:get(K1, Store0),
-	    V2 = gb_trees:get(K2, Store0),
-	    Hue = gb_trees:get(hue, Store0),
-	    {Va,Vb,Vc} = color_update(T, Val, V1, V2, Hue),
-	    Store1 = gb_trees:update(Var, Val, Store0),
-	    Store2 = gb_trees:update(Ka, Va, Store1),
-	    Store3 = gb_trees:update(Kb, Vb, Store2),
-	    Store = gb_trees:update(Kc, Vc, Store3),
-	    {store,Store};
-	(_, _) ->
-	    void
+update_element(Col, V, RGB) ->
+    case element_no(Col) of
+	{rgb, Index} -> setelement(Index, RGB, V);
+	{hsv, Index} -> hsv_to_rgb(setelement(Index, rgb_to_hsv(RGB), V))
     end.
 
-eyepicker_update(update, {Var,_I,{R,G,B}=Col,Sto0}) ->
-    Sto1 = gb_trees:update(Var, Col, Sto0),
-    Sto2 = gb_trees:update(red, R, Sto1),
-    Sto3 = gb_trees:update(green, G, Sto2),
-    Sto4 = gb_trees:update(blue, B, Sto3),
-    {H,S,V} = rgb_to_hsv(R, G, B),
-    Sto5 = gb_trees:update(hue, H, Sto4),
-    Sto6 = gb_trees:update(sat, S, Sto5),
-    Sto = gb_trees:update(val, V, Sto6),
-    {store,Sto};
-eyepicker_update(_, _) -> void.
+get_element(Col, RGB) ->
+    case element_no(Col) of
+	{rgb, Index} -> element(Index, RGB);
+	{hsv, Index} -> element(Index, rgb_to_hsv(RGB))
+    end.
 
-color_update(r, R, G, B, OldHue) ->
-    rgb_to_hsv(R, G, B, OldHue);
-color_update(g, G, R, B, OldHue) ->
-    rgb_to_hsv(R, G, B, OldHue);
-color_update(b, B, R, G, OldHue) ->
-    rgb_to_hsv(R, G, B, OldHue);
-color_update(h, H, S, V, _) ->
-    hsv_to_rgb(H, S, V);
-color_update(s, S, H, V, _) ->
-    hsv_to_rgb(H, S, V);
-color_update(v, V, H, S, _) ->
-    hsv_to_rgb(H, S, V).
+set_palette(undefined, Data) ->
+    DefPalette = [{G,G,G} || G <- lists:seq(0, 255, 255 div 15)],
+    set_palette(DefPalette, Data, 0);
+set_palette(Get, Data) when is_function(Get) ->
+    set_palette(Get(), Data, 0).
+
+set_palette([Col|Pal], Data, I) when I < 16 ->
+    wxColourData:setCustomColour(Data, I, wings_color:rgb3bv(Col)),
+    set_palette(Pal, Data, I+1);
+set_palette(_, _, _) -> ok.
+
+get_palette(Data) ->
+    get_palette(Data, 0).
+
+get_palette(Data, I) when I < 16 ->
+    [rgb3fv(wxColourData:getCustomColour(Data,I))|get_palette(Data, I+1)];
+get_palette(_, _) -> [].
+
+%% eyepicker_update(update, {Var,_I,{R,G,B}=Col,Sto0}) ->
+%%     Sto1 = gb_trees:update(Var, Col, Sto0),
+%%     Sto2 = gb_trees:update(red, R, Sto1),
+%%     Sto3 = gb_trees:update(green, G, Sto2),
+%%     Sto4 = gb_trees:update(blue, B, Sto3),
+%%     {H,S,V} = rgb_to_hsv(R, G, B),
+%%     Sto5 = gb_trees:update(hue, H, Sto4),
+%%     Sto6 = gb_trees:update(sat, S, Sto5),
+%%     Sto = gb_trees:update(val, V, Sto6),
+%%     {store,Sto};
+%% eyepicker_update(_, _) -> void.

src/wings_console.erl
@@ -363,8 +363,8 @@ wc_open_window(#state{lines=Lines}=State, Opts) ->
     Ctrl = wxTextCtrl:new(Win, ?wxID_ANY, [{style, Style}]),
 
     wxWindow:setFont(Ctrl, Font),
-    wxWindow:setBackgroundColour(Ctrl, wings_util:col4bv(wings_pref:get_value(console_color))),
-    wxWindow:setForegroundColour(Ctrl, wings_util:col4bv(wings_pref:get_value(console_text_color))),
+    wxWindow:setBackgroundColour(Ctrl, wings_color:rgb4bv(wings_pref:get_value(console_color))),
+    wxWindow:setForegroundColour(Ctrl, wings_color:rgb4bv(wings_pref:get_value(console_text_color))),
     wxTextCtrl:appendText(Ctrl, [[Line,$\n] || Line <- queue:to_list(Lines)]),
     wxFrame:show(Win),
     wxFrame:connect(Win, close_window, [{skip, true}]),

commit 8225c1c74ea97cb0e644a74db341a4d0ce88123e

Fix broken color slider hook
Became broken in 5f8c96f9 when I changed the hook
implementation for a combined color slider with a color ctrl.

src/wings_dialog.erl
@@ -331,7 +331,11 @@ set_value_impl(#in{wx=Ctrl, type=slider, data={_, ToSlider}}, Val, _) ->
     wxSlider:setValue(Ctrl, ToSlider(Val));
 set_value_impl(#in{wx=Ctrl, type=col_slider}, Val, _) ->
     ww_color_slider:setColor(Ctrl, Val);
-set_value_impl(#in{wx=Ctrl, type=color}, Val, _) ->
+set_value_impl(#in{wx=Ctrl, type=color, wx_ext=Ext}, Val, _) ->
+    case Ext of
+	[Slider] -> ww_color_slider:setColor(Slider, Val);
+	_ -> ignore
+    end,
     ww_color_ctrl:setColor(Ctrl, Val);
 set_value_impl(In=#in{type=button}, Val, Store) ->
     true = ets:insert(Store, In#in{data=Val});
@@ -601,11 +605,15 @@ setup_hook(#in{key=Key, wx=Ctrl, type=button, hook=UserHook}, Fields) ->
 				 end}]),
     ok;
 
-setup_hook(#in{key=Key, wx=Ctrl, type=color, hook=UserHook}, Fields) ->
-    ww_color_ctrl:connect(Ctrl, col_changed,
-			  [{callback, fun({col_changed, Col}) ->
-					      UserHook(Key, Col, Fields)
-				      end}]),
+setup_hook(#in{key=Key, wx=Ctrl, type=color, wx_ext=Ext, hook=UserHook}, Fields) ->
+    CB = fun({col_changed, Col}) ->
+		 UserHook(Key, Col, Fields)
+	 end,
+    ww_color_ctrl:connect(Ctrl, col_changed, [{callback, CB}]),
+    case Ext of
+	[Slider] -> ww_color_slider:connect(Slider, col_changed, [{callback, CB}]);
+	_ -> ignore
+    end,
     ok;
 setup_hook(#in{key=Key, wx=Ctrl, type=col_slider, hook=UserHook}, Fields) ->
     ww_color_slider:connect(Ctrl, col_changed,