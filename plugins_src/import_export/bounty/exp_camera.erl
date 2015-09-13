14dac0319f22806aa52f5b643762f0ecf2b7fd5a
povmaniac <thebountyrenderer@gmail.com>
2015-09-13 08:54:17 +0200

    Cleanup and indent export camera code

--- plugins_src/import_export/bounty/exp_camera.erl
+++ plugins_src/import_export/bounty/exp_camera.erl
@@ -4,79 +4,50 @@
 %
 
 export_camera(F, Name, Attr) ->
-    #camera_info{pos=Pos,dir=Dir,up=Up,fov=Fov} =
-        proplists:lookup(camera_info, Attr),
+    #camera_info{pos=Pos,dir=Dir,up=Up,fov=Fov} = proplists:lookup(camera_info, Attr),
     Width = proplists:get_value(width, Attr),
     Height = proplists:get_value(height, Attr),
     Lens_Type = proplists:get_value(lens_type, Attr),
-    Lens_Ortho_Scale = proplists:get_value(lens_ortho_scale, Attr),
-    Lens_Angular_Circular = proplists:get_value(lens_angular_circular, Attr),
-    Lens_Angular_Mirrored = proplists:get_value(lens_angular_mirrored, Attr),
-    Lens_Angular_Max_Angle = proplists:get_value(lens_angular_max_angle, Attr),
-    Lens_Angular_Angle = proplists:get_value(lens_angular_angle, Attr),
     Ro = math:pi()/180.0,
     %% Fov is vertical angle from lower to upper border.
-    %% YafaRay focal plane is 1 unit wide.
+    %% TheBounty focal plane is 1 unit wide.
     FocalDist = 0.5 / ((Width/Height) * math:tan(limit_fov(Fov)*0.5*Ro)),
     Aperture = proplists:get_value(aperture, Attr),
-    println(F, "<camera name=\"~s\"> "++
-                "<resx ival=\"~w\"/> <resy ival=\"~w\"/> <focal fval=\"~.10f\"/>"++
-                if Aperture > 0.0 ->
-                        "~n        <dof_distance fval=\"~.10f\"/> <aperture fval=\"~.10f\"/>"
-                            "~n        <use_qmc bval=\"~s\"/> <bokeh_type sval=\"~s\"/>"
-                            "~n        <bokeh_bias sval=\"~s\"/> <bokeh_rotation fval=\"~.10f\"/> <dof_distance fval=\"~.10f\"/>";
-                   true -> ""
-                end++
-
-
-                case Lens_Type of
-                    perspective   ->
-                        "~n <type sval=\"~s\"/>";
-
-                    orthographic  ->
-                        "~n <type sval=\"~s\"/>"
-                            "<scale fval=\"~.10f\"/>";
-
-                    architect     ->
-                        "~n <type sval=\"~s\"/>";
-
-                    angular           ->
-                        "~n <type sval=\"~s\"/>"
-                            "<circular bval=\"~s\"/>"
-                            "<mirrored bval=\"~s\"/>"
-                            "~n <max_angle fval=\"~.10f\"/>"
-                            "<angle fval=\"~.10f\"/>"
-
-                end,
-
-
-            [Name,Width,Height,FocalDist]++
-                if Aperture > 0.0 ->
-                        [e3d_vec:len(Dir),
-                         Aperture,
-                         format(proplists:get_value(bokeh_use_QMC, Attr)),
-                         format(proplists:get_value(bokeh_type, Attr)),
-                         format(proplists:get_value(bokeh_bias, Attr)),
-                         proplists:get_value(bokeh_rotation, Attr),
-                         proplists:get_value(dof_distance, Attr)];
-                   true -> []
-                end++
-
-
-                case Lens_Type of
-                    perspective -> [format(Lens_Type)];
-                    orthographic  -> [format(Lens_Type),Lens_Ortho_Scale];
-
-                    architect    -> [format(Lens_Type)];
-                    angular    -> [format(Lens_Type),
-                                   format(Lens_Angular_Circular),
-                                   format(Lens_Angular_Mirrored),
-                                   Lens_Angular_Max_Angle,
-                                   Lens_Angular_Angle]
-
-                end
-
-           ),
+	
+    println(F, "<camera name=\"~s\">",[Name]),
+	println(F, "\t<type sval=\"~s\"/>",[Lens_Type]),
+	println(F, "\t<resx ival=\"~w\"/>",[Width]),
+	println(F, "\t<resy ival=\"~w\"/>",[Height]),
+	println(F, "\t<focal fval=\"~.10f\"/>",[FocalDist]),
+    case Aperture of
+		0.0 ->
+			ok;
+		_ ->
+			println(F, "\t<aperture fval=\"~.10f\"/>",[Aperture]),
+			%println(F, "\t<use_qmc bval=\"~s\"/>",[format(proplists:get_value(bokeh_use_QMC, Attr))]),
+			println(F, "\t<bokeh_type sval=\"~s\"/>",[format(proplists:get_value(bokeh_type, Attr))]),
+			println(F, "\t<bokeh_bias sval=\"~s\"/>",[format(proplists:get_value(bokeh_bias, Attr))]),
+			println(F, "\t<bokeh_rotation fval=\"~.10f\"/>",[proplists:get_value(bokeh_rotation, Attr)]),
+			println(F, "\t<dof_distance fval=\"~.10f\"/>",[proplists:get_value(dof_distance, Attr)])
+    end,
+	Lens_Type = proplists:get_value(lens_type, Attr),
+    case Lens_Type of
+        %
+        orthographic ->
+            println(F,
+                "\t<scale fval=\"~.10f\"/>",[proplists:get_value(lens_ortho_scale, Attr)]);
+        angular ->
+            println(F,
+                "\t<circular bval=\"~s\"/>",[proplists:get_value(lens_angular_circular, Attr)]),
+            println(F,
+                "\t<mirrored bval=\"~s\"/>",[proplists:get_value(lens_angular_mirrored, Attr)]),
+            println(F,
+                "\t<max_angle fval=\"~.10f\"/>",[proplists:get_value(lens_angular_max_angle, Attr)]),
+            println(F,
+                "\t<angle fval=\"~.10f\"/>",[proplists:get_value(lens_angular_angle, Attr)]);
+        _ -> ok
+    end,
+    
     export_pos(F, from, Pos),
     export_pos(F, to, e3d_vec:add(Pos, Dir)),
     export_pos(F, up, e3d_vec:add(Pos, Up)),
