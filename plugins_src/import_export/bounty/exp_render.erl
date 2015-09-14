f6cb4475a618ec2d6b1466c4104720e30afec385
povmaniaco <thebountyrenderer@gmail.com>
2015-09-14 22:42:18 +0200

    Star to re-write and fix integrator ans render code.

--- plugins_src/import_export/bounty/exp_render.erl
+++ plugins_src/import_export/bounty/exp_render.erl
@@ -36,7 +36,7 @@ export_render(F, CameraName, BackgroundName, Outfile, Attr) ->
     Do_AO = proplists:get_value(do_ao, Attr),
     AO_Distance = proplists:get_value(ao_distance, Attr),
     AO_Samples = proplists:get_value(ao_samples, Attr),
-    AO_Color = 	proplists:get_value(ao_color,Attr),
+    AO_Color =  proplists:get_value(ao_color,Attr),
     Lighting_Method = proplists:get_value(lighting_method, Attr),
     PM_Diffuse_Photons = proplists:get_value(pm_diffuse_photons, Attr),
     PM_Bounces = proplists:get_value(pm_bounces, Attr),
@@ -69,24 +69,37 @@ export_render(F, CameraName, BackgroundName, Outfile, Attr) ->
     Volintegr_Stepsize = proplists:get_value(volintegr_stepsize, Attr),
     ThreadsAuto = proplists:get_value(threads_auto, Attr),
     ThreadsNumber = proplists:get_value(threads_number, Attr),
-    println(F," "),
+    
+    
     println(F, "<integrator name=\"default\">"),
+    println(F, "<raydepth ival=\"~w\"/>",[Raydepth]),
+    println(F, "<transpShad bval=\"~s\"/>",[format(TransparentShadows)]),
+    println(F, "<shadowDepth ival=\"~w\"/>",[ShadowDepth]),
 
     case Lighting_Method of
         directlighting ->
-            println(F," "),
             println(F, "<type sval=\"~s\"/>",[Lighting_Method]),
-            println(F, "<raydepth ival=\"~w\"/>",[Raydepth]),
-            println(F, "<transpShad bval=\"~s\"/>",[format(TransparentShadows)]),
-            println(F, "<shadowDepth ival=\"~w\"/>",[ShadowDepth]),
-            println(F," ");
+            println(F, "<caustics bval=\"~s\"/>",[UseCaustics]),
+            case UseCaustics of
+                    true ->            
+                        println(F, "<photons ival=\"~w\"/>",[Caustic_Photons]),
+                        println(F, "<caustic_depth ival=\"~w\"/>",[Caustic_Depth]),
+                        println(F, "<caustic_mix ival=\"~w\"/>",[Caustic_Mix]),
+                        println(F, "<caustic_radius fval=\"~.10f\"/>",[Caustic_Radius]);
+
+                    false -> println(F,"")
+            end,
+            println(F, "<do_AO bval=\"~s\"/>",[Do_AO]),
+            case Do_AO of
+                    true ->
+                        println(F, "\t<AO_distance fval=\"~.10f\"/>",[AO_Distance]),
+                        println(F, "<AO_samples fval=\"~.10f\"/>",[AO_Samples]),
+                        export_rgb(F, "AO_color",AO_Color);
+                    false -> println(F, "")
+            end;
 
         photonmapping ->
-            println(F," "),
             println(F, "<type sval=\"~s\"/>",[Lighting_Method]),
-            println(F, "<raydepth ival=\"~w\"/>",[Raydepth]),
-            println(F, "<transpShad bval=\"~s\"/>",[format(TransparentShadows)]),
-            println(F, "<shadowDepth ival=\"~w\"/>",[ShadowDepth]),
             println(F, "<photons ival=\"~w\"/>",[PM_Diffuse_Photons]),
             println(F, "<bounces ival=\"~w\"/>",[PM_Bounces]),
             println(F, "<search ival=\"~w\"/>",[PM_Search]),
@@ -97,32 +110,24 @@ export_render(F, CameraName, BackgroundName, Outfile, Attr) ->
             println(F, "<finalGather bval=\"~s\"/>",[PM_Use_FG]),
             println(F, "<fg_bounces ival=\"~w\"/>",[PM_FG_Bounces]),
             println(F, "<fg_samples ival=\"~w\"/>",[PM_FG_Samples]),
-            println(F, "<show_map bval=\"~s\"/>",[PM_FG_Show_Map]),
-            println(F," ");
+            println(F, "<show_map bval=\"~s\"/>",[PM_FG_Show_Map]);
+            %println(F," ");
 
         pathtracing ->
-            println(F," "),
             println(F, "<type sval=\"~s\"/>",[Lighting_Method]),
-            println(F, "<raydepth ival=\"~w\"/>",[Raydepth]),
-            println(F, "<transpShad bval=\"~s\"/>",[format(TransparentShadows)]),
-            println(F, "<shadowDepth ival=\"~w\"/>",[ShadowDepth]),
             println(F, "<photons ival=\"~w\"/>",[PT_Diffuse_Photons]),
+            %% TO DO: review case when not caustic is active
             println(F, "<bounces ival=\"~w\"/>",[PT_Bounces]),
             println(F, "<caustic_type sval=\"~s\"/>",[PT_Caustic_Type]),
             println(F, "<caustic_radius fval=\"~.10f\"/>",[PT_Caustic_Radius]),
             println(F, "<caustic_mix ival=\"~w\"/>",[PT_Caustic_Mix]),
             println(F, "<caustic_depth ival=\"~w\"/>",[PT_Caustic_Depth]),
-            println(F, "<path_samples ival=\"~w\"/>",[PT_Samples]),
-            println(F," ");
+            println(F, "<path_samples ival=\"~w\"/>",[PT_Samples]);
 
         bidirectional ->
-            println(F," "),
-            println(F, "<type sval=\"~s\"/>",[Lighting_Method]),
-            println(F, "<raydepth ival=\"~w\"/>",[Raydepth]),
-            println(F," ");
+            println(F, "<type sval=\"~s\"/>",[Lighting_Method]);
 
         sppm ->
-            println(F," "),
             println(F, "<type sval=\"SPPM\"/>"),
             println(F, "<photons ival=\"~w\"/>",[SPPM_Photons]),
             println(F, "<bounces ival=\"~w\"/>",[SPPM_Bounces]),
@@ -130,49 +135,23 @@ export_render(F, CameraName, BackgroundName, Outfile, Attr) ->
             println(F, "<photonRadius fval=\"~.10f\"/>",[SPPM_Radius]),
             println(F, "<times fval=\"~.10f\"/>",[SPPM_Times]),
             println(F, "<passNums ival=\"~w\"/>",[SPPM_Passes]),
-            println(F, "<pmIRE bval=\"~s\"/>",[format(SPPM_Ire)]),
-            println(F," ")
+            println(F, "<pmIRE bval=\"~s\"/>",[format(SPPM_Ire)])
 
     end,
 
+    case Lighting_Method of
+        {pathtracing,photonmapping,directlighting} ->
+            case  UseSSS of
+                true ->
+                    println(F, "<useSSS bval=\"true\"/>"),
+                    println(F, "<sssPhotons ival=\"~w\"/>",[SSS_Photons]),
+                    println(F, "<sssDepth ival=\"~w\"/>",[SSS_Depth]),
+                    println(F, "<sssScale fval=\"~.10f\"/>",[SSS_Scale]),
+                    println(F, "<singleScatterSamples ival=\"~w\"/>",[SSS_SingleScatter_Samples]);
 
-    case UseCaustics of
-        true ->
-            println(F, "<caustics bval=\"true\"/>"),
-            println(F, "<photons ival=\"~w\"/>",[Caustic_Photons]),
-            println(F, "<caustic_depth ival=\"~w\"/>",[Caustic_Depth]),
-            println(F, "<caustic_mix ival=\"~w\"/>",[Caustic_Mix]),
-            println(F, "<caustic_radius fval=\"~.10f\"/>",[Caustic_Radius]);
-
-        false ->
-            println(F, "<caustics bval=\"false\"/>")
-
-    end,
-
-    case Do_AO of
-        true ->
-            println(F, "<do_AO bval=\"true\"/>"),
-            println(F, "<AO_distance fval=\"~.10f\"/>",[AO_Distance]),
-            println(F, "<AO_samples fval=\"~.10f\"/>",[AO_Samples]),
-            export_rgb(F, "AO_color",AO_Color);
-
-
-        false ->
-            println(F, "<do_AO bval=\"false\"/>")
-
-    end,
-
-    case UseSSS of
-        true ->
-            println(F, "<useSSS bval=\"true\"/>"),
-            println(F, "<sssPhotons ival=\"~w\"/>",[SSS_Photons]),
-            println(F, "<sssDepth ival=\"~w\"/>",[SSS_Depth]),
-            println(F, "<sssScale fval=\"~.10f\"/>",[SSS_Scale]),
-            println(F, "<singleScatterSamples ival=\"~w\"/>",[SSS_SingleScatter_Samples]);
-
-        false ->
-            println(F, "<ibl bval=\"false\"/>")
-
+                false -> ""
+            end;
+        _ -> ""
     end,
 
     println(F, "</integrator>"),
@@ -200,59 +179,52 @@ export_render(F, CameraName, BackgroundName, Outfile, Attr) ->
         case RenderFormat of
             exr ->
                 [if ExrFlagFloat -> "float "; true -> "" end,
-		 if ExrFlagZbuf -> "zbuf "; true -> "" end,
-		 format(ExrFlagCompression)];
+                 if ExrFlagZbuf -> "zbuf "; true -> "" end,
+                 format(ExrFlagCompression)];
             _ -> ""
         end,
-    println(F, "<render> <camera_name sval=\"~s\"/> "
-    "<filter_type sval=\"~s\"/>"
-    "<AA_passes ival=\"~w\"/>~n"
-    "        <AA_threshold fval=\"~.10f\"/>~n"
-    "        <AA_minsamples ival=\"~w\"/> <AA_pixelwidth fval=\"~.10f\"/>~n"++
-        case SaveAlpha of
-            premultiply ->
-                "        <premult bval=\"true\"/>~n";
-            backgroundmask ->
-                "        alpha_backgroundmask=\"on\"/~n";
+    println(F, "<render>"),
+    println(F, "\t<camera_name sval=\"~s\"/>",[CameraName]),
+    println(F, "\t<filter_type sval=\"~s\"/>",[AA_Filter_Type]),
+    println(F, "\t<AA_passes ival=\"~w\"/>",[AA_passes]),
+    println(F, "\t<AA_threshold fval=\"~.10f\"/>",[AA_threshold]),
+    println(F, "\t<AA_minsamples ival=\"~w\"/>",[AA_minsamples]),
+    println(F, "\t<AA_pixelwidth fval=\"~.10f\"/>",[AA_pixelwidth]),
+    case SaveAlpha of
+        premultiply ->
+            println(F, "\t<tpremult bval=\"true\"/>");
+        backgroundmask ->
+                println(F, "\talpha_backgroundmask=\"on\"/~n");
             _ -> ""
-        end++
-        "        <clamp_rgb bval=\"~s\"/>~n"
-        "        <bg_transp_refract bval=\"~s\"/>~n"
-        "    <background_name sval=\"~s\"/>~n"++
+        end,
+        println(F, "\t<clamp_rgb bval=\"~s\"/>",[format(ClampRGB)]),
+        println(F, "\t<bg_transp_refract bval=\"~s\"/>",[format(BackgroundTranspRefract)]),
+        println(F, "\t<background_name sval=\"~s\"/>",[BackgroundName]),
+        %
+        println(F, "\t<output_type sval=\"~s\"/>",[RenderFormat]),
         case RenderFormat of
-            tga -> "";
-            _   -> "    <output_type sval=\"~s\"/>~n"
-        end++
-        case RenderFormat of
-            exr -> "    <exr_flags sval=\"~s\"/>~n";
+            exr -> println(F, "\t<exr_flags sval=\"~s\"/>",[ExrFlags]);
             _   -> ""
-        end++
+        end,
 
-        "    <width ival=\"~w\"/> <height ival=\"~w\"/>~n"
-        "    <outfile sval=\"~s\"/>~n"
-        "    <indirect_samples sval=\"0\"/>~n"
-        "    <indirect_power sval=\"1.0\"/>~n"
-        "    <exposure fval=\"~.10f\"/>~n"++
-        case SaveAlpha of
-            false -> "";
-            _ ->
-                "    <save_alpha bval=\"on\"/>~n"
-        end++
-        "    <gamma fval=\"~.10f\"/>~n"
-        "    ",
-        [CameraName,AA_Filter_Type,AA_passes,AA_threshold,
-            AA_minsamples,AA_pixelwidth,
-            format(ClampRGB),format(BackgroundTranspRefract),BackgroundName]++
-            case RenderFormat of
-                tga -> [];
-                _   -> [format(RenderFormat)]
-            end++
-            case RenderFormat of
-                exr -> [ExrFlags];
-                _   -> []
-            end++
-
-            [Width,Height,Outfile,Exposure,Gamma]),
+        println(F, "\t<width ival=\"~w\"/>",[Width]), 
+        println(F, "\t<height ival=\"~w\"/>",[Height]),
+        println(F, "\t<outfile sval=\"~s\"/>",[Outfile]),
+        %println(F, "\t<indirect_samples sval=\"0\"/>"
+        %println(F, "\t<indirect_power sval=\"1.0\"/>"
+        println(F, "\t<exposure fval=\"~.10f\"/>",[Exposure]),
+        println(F, "\t<save_alpha bval=\"~s\"/>",[format(SaveAlpha)]),
+        %end,
+        println(F, "\t<gamma fval=\"~.10f\"/>",[Gamma]),
+        
+        %case RenderFormat of
+        %        tga -> [];
+        %        _   -> [format(RenderFormat)]
+        %    end,
+        %    case RenderFormat of
+        %        exr -> [ExrFlags];
+        %        _   -> []
+        %    end,
 
     println(F, "<integrator_name sval=\"default\"/>"),
 
