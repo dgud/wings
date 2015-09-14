d36c8cf61731160b380bb25233a98ba8711ca267
Dan Gudmundsson <dgud@users.sourceforge.net>
2015-09-14 23:21:40 +0200

    Rework palette window to wx
    Does not do drag and drop yet

--- src/wings_dialog.erl
+++ src/wings_dialog.erl
@@ -724,7 +724,9 @@ setup_hook(_What, _) ->
 return_result(Fun, Values, Owner) ->
     case Fun(Values) of
 	ignore ->
-	    ok;
+	    keep;
+	{return, Result} ->
+	    Result;
 	#st{}=St ->
 	    wings_wm:send(Owner, {new_state,St});
 	{commit,#st{}=St0,#st{}=St}->
@@ -735,8 +737,7 @@ return_result(Fun, Values, Owner) ->
 	    wings_wm:send_after_redraw(Owner, {action,Action});
 	Action when is_tuple(Action); is_atom(Action) ->
 	    wings_wm:send(Owner, {action,Action})
-    end,
-    keep.
+    end.
 
 to_label_column(Qs) ->
     lists:map(fun label_col/1, Qs).
