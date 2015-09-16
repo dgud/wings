477eb53d8b05ecd3662dfc2fbda4de513a39e185
povmaniaco <thebountyrenderer@gmail.com>
2015-09-16 02:28:37 +0200

    Begin re-factory modulators code. Some parts are incomplete. Need more review and test.

--- plugins_src/import_export/bounty/exp_modulators.erl
+++ plugins_src/import_export/bounty/exp_modulators.erl
@@ -1,216 +1,262 @@
-%%%%
+%
+%
+%
+%
 
 export_modulator(F, Texname, Maps, {modulator,Ps}, _Opacity) when is_list(Ps) ->
     case mod_enabled_mode_type(Ps, Maps) of
         {false,_,_} ->
             off;
-        {true,Mode,Type} ->
+        {true,BlendMode,TexType} ->
 
-            AlphaIntensity = proplists:get_value(alpha_intensity, Ps, ?DEF_MOD_ALPHA_INTENSITY),
-
-            %%% Start Change Number from Texname for UpperLayer
-
-            UpperLayerName =
-                case AlphaIntensity of
-                    stencil -> re:replace(Texname,"_2","_1",[global]);
-                    _-> re:replace(Texname,"_1","_2",[global])
-                end,
-
-            %%% Start Change Number from Texname for Stencil Input
-
-            StencilInputName =
-                case AlphaIntensity of
-                    stencil -> re:replace(Texname,"_2","_3",[global]);
-                    _-> ""
-                end,
-
-%%% Start Change Number from Texname for Stencil UpperLayer Name 2
-
-            StencilUpperLayerName2 =
-                case AlphaIntensity of
-                    stencil -> re:replace(Texname,"_1","_2",[global]);
-                    _-> ""
-                end,
-
-
-%            _SizeX = proplists:get_value(size_x, Ps, ?DEF_MOD_SIZE_X),
-%            _SizeY = proplists:get_value(size_y, Ps, ?DEF_MOD_SIZE_Y),
-%            _SizeZ = proplists:get_value(size_z, Ps, ?DEF_MOD_SIZE_Z),
-            TextureType = proplists:get_value(texture_type, Ps, ?DEF_TEXTURE_TYPE),
-            Diffuse = proplists:get_value(diffuse, Ps, ?DEF_MOD_DIFFUSE),
-%%      _Specular = proplists:get_value(specular, Ps, ?DEF_MOD_SPECULAR),
-%%      Ambient = proplists:get_value(ambient, Ps, ?DEF_MOD_AMBIENT),
-            Shininess = proplists:get_value(shininess, Ps, ?DEF_MOD_SHININESS),
-            Normal = proplists:get_value(normal, Ps, ?DEF_MOD_NORMAL),
-%%            _Color = Diffuse * Opacity,
-%%            _HardValue = Shininess,
-%%            _Transmission = Diffuse * (1.0 - Opacity),
-%%      _Reflection = Ambient,
+            %AlphaIntensity = proplists:get_value(alpha_intensity, Ps, ?DEF_MOD_ALPHA_INTENSITY),
+            %UpperLayerName =  case AlphaIntensity of
+            %        stencil -> re:replace(Texname,"_2","_1",[global]);
+            %        _-> re:replace(Texname,"_1","_2",[global])
+            %    end,
+            %StencilInputName =
+            %    case AlphaIntensity of
+            %        stencil -> re:replace(Texname,"_2","_3",[global]);
+            %        _-> ""
+            %    end,
+            %StencilUpperLayerName2 =
+            %    case AlphaIntensity of
+            %        stencil -> re:replace(Texname,"_1","_2",[global]);
+            %        _-> ""
+            %    end,
+            %-----------------------------------------------------------------------
+            % povman: change TextureType variable to most clarified ShaderType
+            % TextureType = proplists:get_value(texture_type, Ps, ?DEF_TEXTURE_TYPE),
+            %-----------------------------------------------------------------------
+			
+            ShaderType = proplists:get_value(texture_type, Ps, ?DEF_SHADER_TYPE),
             TexCo =
-                case Type of
-                    {image,jpeg,map,_}-> "<texco sval=\"uv\"/>";
-                    {marble, wood,clouds} -> "<texco sval=\"global\"/>";
-                    _ -> ""
+                case TexType of
+                    {image,jpeg,map,_}-> "uv"; %"<texco sval=\"uv\"/>";
+                    {marble, wood,clouds} -> "global"; %"<texco sval=\"global\"/>";
+                    _ -> "uv"
                 end,
 
-            ModeNumber =
-                case Mode of
-                    %mix -> "0"; % test..
-                    add -> "1";
-                    mul -> "2";
-                    sub -> "3";
-                    scr -> "4";
-                    divide -> "5";
-                    dif -> "6";
-                    dar -> "7";
-                    lig -> "8";
-                    _ -> "0"
-                end,
-
-%% Start Identify Modulator # (w_default_Name_1 or w_default_Name_2)
+            %% Start Identify Modulator # (w_default_Name_1 or w_default_Name_2)
             Split=re:split(Texname,"_",[{return, list}]),
             Num=lists:last(Split),
-            UpperLayer =
-                case {Num,Mode,AlphaIntensity} of
-                    {"1",mix,_} ->  "";
-                    {"1",_,_} ->  "<upper_layer sval=\""++UpperLayerName++"\"/>";
-                    {"2",_,stencil} ->  "<upper_layer sval=\""++UpperLayerName++"\"/>";
+            %UpperLayer =
+            %    case {Num,BlendMode,AlphaIntensity} of
+            %        {"1",mix,_} ->  "";
+            %        {"1",_,_} ->  "<upper_layer sval=\""++UpperLayerName++"\"/>";
+            %        {"2",_,stencil} ->  "<upper_layer sval=\""++UpperLayerName++"\"/>";
+            %        _ -> ""
+            %    end,
+            %% End Identify Modulator #
+            %UseAlpha =
+            %    case {Num,AlphaIntensity} of
+            %        {"1",off} ->  "";
+            %        {_,transparency} -> "<do_scalar bval=\"true\"/>";
+            %        {_,diffusealphatransparency} -> "<use_alpha bval=\"true\"/>";
+            %        {_,translucency} -> "<do_scalar bval=\"true\"/>";
+            %        {_,specularity} -> "<do_scalar bval=\"true\"/>";
+            %        {_,stencil} -> "<use_alpha bval=\"true\"/>";
+            %        _ -> ""
+            %    end,
+            % TextureShaderType            
+
+            %ShaderName =
+            %    case {Num,BlendMode} of
+            %        {"1",_} ->   "  "++TextureShaderType++" sval=\""++Texname++"\"/>";
+            %        {_,mix} ->   "  "++TextureShaderType++" sval=\""++Texname++"\"/>";
+            %        _ -> ""
+            %    end,
+
+            %%------------------------------------------------------------------------------------->
+
+            %% Identify Modulator # (w_default_Name_1 or w_default_Name_2)
+            Split=re:split(Texname,"_",[{return, list}]),
+            Num=lists:last(Split),
+
+            %DoColor = case ShaderType of
+            %        {diffuse, mirror_color, glossy_reflect, glossy} -> true;                    
+            %        {mirror, transparency, translucency, bump}-> false                    
+            %    end,
+			
+			DoColor = true,
+
+            %
+            %_ShaderName =
+            %    case {Num, BlendMode} of
+            %            {"1",_} ->   "  "++ShaderType++" sval=\""++Texname++"\"/>";
+            %            {_,mix} ->   "  "++ShaderType++" sval=\""++Texname++"\"/>";
+            %            _ -> ""
+            %    end,
+
+            %% write shader type. TODO: search better way..
+            %println(F, ShaderType),
+            println(F, "\t<~s_shader sval=\"~s\"/>",[ShaderType,Texname]),
+
+            %% entry to element shader list
+            println(F,"\t<list_element>"),
+
+            %% shader factor amount controled with 'Factor Modulator' slider in UI
+            Factor = proplists:get_value(mod_colorfactor, Ps, ?DEF_MOD_COLORFACTOR),
+            %% Try use value or color
+            FactorType =
+                case DoColor of
+                    true -> "colfac";
+                    _ ->    "valfac"
+                end,
+            %
+            println(F,"\t\t<~s fval=\"~w\"/>",[FactorType,Factor]),
+            %
+            CellType = intensity, %proplists:get_value(cell_type, Ps, ?DEF_MOD_CELLTYPE),
+            %%--------------------------->
+            erlang:display("Texture type is :"++TexType),
+
+            IsColor = % revise
+                case TexType of
+                    image -> true;
+                    {map,_} -> true;
+                    voronoi -> 
+                        case CellType of
+                            intensity -> true;
+                            _ -> false % maybe this case.. fail
+                        end;
+                    _ -> false
+                end,
+
+            println(F,"\t\t<color_input bval=\"~s\"/>",[IsColor]),
+
+            % def color for 'blended' by default
+            println(F,"\t\t<def_col r=\"0.81\" g=\"0.8\" b=\"0.81\" a=\"1\"/>"),
+            %
+            println(F,"\t\t<def_val fval=\"1\"/>"),
+
+            % swich to use texture values
+            %
+            println(F,"\t\t<do_color bval=\"~s\"/>",[DoColor]),
+            %
+            println(F,"\t\t<do_scalar bval=\"~s\"/>",[not DoColor]),
+            %
+            println(F,"\t\t<element sval=\"shader_node\"/>"),
+            %
+            println(F,"\t\t<input sval=\"~s_mod\"/>",[Texname]),
+            %
+            erlang:display("BlendMode for Modenumber "++format(BlendMode)),
+            ModeNumber =
+                case BlendMode of
+                    mix -> "0"; add -> "1"; mul -> "2"; sub -> "3"; scr -> "4";
+                    divide -> "5"; dif -> "6"; dar -> "7"; lig -> "8";
                     _ -> ""
                 end,
-%% End Identify Modulator #
-
-            UpperColor =
-                case Num of
-                    "1" ->  "<upper_color r=\"1\" g=\"1\" b=\"1\" a=\"1\"/>";
-                    _ -> ""
-                end,
-
-            UseAlpha =
-                case {Num,AlphaIntensity} of
-                    {"1",off} ->  "";
-                    {_,transparency} -> "<do_scalar bval=\"true\"/>";
-                    {_,diffusealphatransparency} -> "<use_alpha bval=\"true\"/>";
-                    {_,translucency} -> "<do_scalar bval=\"true\"/>";
-                    {_,specularity} -> "<do_scalar bval=\"true\"/>";
-                    {_,stencil} -> "<use_alpha bval=\"true\"/>";
-                    _ -> ""
-                end,
+            println(F, "\t\t<mode ival=\"~s\"/>",[ModeNumber]),
+            %
+            println(F, "\t\t<name sval=\"~s\"/>",[Texname]),
+            %
+            println(F, "\t\t<negative bval=\"false\"/>"),
+            %
+            println(F, "\t\t<noRGB bval=\"false\"/>"),
+            
+            % _StencilMode = proplists:get_value(stencil_mode, Ps, ?DEF_MOD_STENCIL),
+            % erlang:display("Modulator. Stencil value is: "++StencilMode),
 
 
+            println(F, "\t\t<stencil bval=\"false\"/>"),
 
-            TextureShaderType =
-                case {Normal,TextureType,AlphaIntensity} of
-                    {0.0,diffusetexture,off} -> "<diffuse_shader";
-                    {0.0,mirrorcolortexture,off} -> "<mirror_color_shader";
-                    {0.0,mirrortexture,off} -> "<mirror_shader";
-                    {0.0,glossytexture,off} -> "<glossy_shader";
-                    {0.0,glossyreflecttexture,off} -> "<diffuse_reflect_shader";
-                    {0.0,transparencytexture,off} -> "<transparency_shader";
-                    {0.0,translucencytexture,off} -> "<translucency_shader";
-                    {0.0,bumptexture,off} -> "<bump_shader";
+            % for layers, is need 'stencil' param and more review code.. :)
+            %%
+            %_UpperColor =
+            %    case Num of
+            %        "1" ->  "<upper_color r=\"1\" g=\"1\" b=\"1\" a=\"1\"/>";
+            %        _ -> ""
+            %    end,
 
-                    {0.0,diffusetexture,transparency} -> "<transparency_shader";
-                    {0.0,mirrorcolortexture,transparency} -> "<transparency_shader";
-                    {0.0,mirrortexture,transparency} -> "<transparency_shader";
-                    {0.0,glossytexture,transparency} -> "<transparency_shader";
-                    {0.0,glossyreflecttexture,transparency} -> "<transparency_shader";
-                    {0.0,transparencytexture,transparency} -> "<transparency_shader";
+            %%% Change Number from Texname for UpperLayer
+            %_ULayerName =
+            %    case ModShaderType of % TODO: change for stencil mode
+            %        stencil ->
+            %            re:replace(Texname,"_2","_1",[global]);
+            %        _->
+            %            re:replace(Texname,"_1","_2",[global])
+            %    end,
+            % UpperLayer =
+            %    case {Num, BlendMode, ModShaderType} of
+            %            {"1",mix,_} ->  "";
+            %            {"1",_,_} ->  UlayerName;
+            %            {"2",_,stencil} -> UlayerName;
+            %            _ -> ""
+            %    end,
 
-                    {0.0,diffusetexture,diffusealphatransparency} -> "<diffuse_shader";
-                    {0.0,transparencytexture,diffusealphatransparency} -> "<diffuse_shader";
-
-                    {0.0,diffusetexture,translucency} -> "<translucency_shader";
-                    {0.0,glossytexture,translucency} -> "<translucency_shader";
-                    {0.0,translucencytexture,translucency} -> "<translucency_shader";
-
-                    {0.0,diffusetexture,specularity} -> "<mirror_shader";
-                    {0.0,mirrorcolortexture,specularity} -> "<mirror_shader";
-                    {0.0,mirrortexture,specularity} -> "<mirror_shader";
-                    {0.0,glossytexture,specularity} -> "<mirror_shader";
-                    {0.0,glossyreflecttexture,specularity} -> "<mirror_shader";
-
-                    {0.0,diffusetexture,stencil} -> "<diffuse_shader";
-                    _ -> "<bump_shader"
-                end,
-
-            ShaderName =
-                case {Num,Mode} of
-                    {"1",_} ->   "  "++TextureShaderType++" sval=\""++Texname++"\"/>";
-                    {_,mix} ->   "  "++TextureShaderType++" sval=\""++Texname++"\"/>";
-                    _ -> ""
-                end,
-
-
-            case AlphaIntensity of
-                stencil ->
-%%Stencil Export Start
-                    println(F, " <!--Start Stencil Section Here-->
-
-                <list_element>
-                <element sval=\"shader_node\"/>
-                <name sval=\"~s\"/>
-                <input sval=\"~s_mod\"/>
-
-                <noRGB bval=\"true\"/>
-                <stencil bval=\"true\"/>
-                "++UpperLayer++"
-
-                <type sval=\"layer\"/>
-                <mode ival=\""++ModeNumber++"\"/>
-                </list_element>
-
-                <list_element>
-                <element sval=\"shader_node\"/>
-                <name sval=\"~s_mod\"/>
-                "++TexCo++"
-                <mapping sval=\"plain\"/>
-                <texture sval=\"~s\"/>
-                <type sval=\"texture_mapper\"/>
-                <bump_strength fval=\"~.3f\"/>
-                </list_element>
-
-                <diffuse_shader sval=\"diff_layer2\"/>
-                                <list_element>
-                                <element sval=\"shader_node\"/>
-                                <name sval=\"diff_layer2\"/>
-                <input sval=\""++StencilInputName++"_mod\"/>
-                <upper_layer sval=\""++StencilUpperLayerName2++"\"/>
-                                <type sval=\"layer\"/>
-                <mode ival=\""++ModeNumber++"\"/>
-                                </list_element>
-
-    <!--End Stencil Section Here-->",
-                        [Texname,Texname,Texname,Texname,Normal
-                        ]);
-%%Stencil Export End
+            Ulayer = "",
+            %
+            case Ulayer of
+                "" -> 
+                    case DoColor of
+                        true ->
+                            println(F,
+                                "\t\t<upper_color r=\"1\" g=\"1\" b=\"1\" a=\"1\"/>\n"
+                                "\t\t<upper_value fval=\"0\"/>");
+                        false -> 
+                            println(F,
+                                "\t\t<upper_color r=\"0\" g=\"0\" b=\"0\" a=\"1\"/>\n" % alpha = 1 ??
+                                "\t\t<upper_value fval=\"~w\"/>",[Factor])
+                    end;
                 _ ->
+                    println(F,"\t\t<upper_layer sval=\"~s\"/>",[Ulayer])
+            end,
 
-                    println(F, "  "++ShaderName++"
-                <list_element>
-                <element sval=\"shader_node\"/>
-                <name sval=\"~s\"/>
-                <input sval=\"~s_mod\"/>
-                                "++UpperLayer++"
-                "++UpperColor++"
-                "++UseAlpha++"
-                <type sval=\"layer\"/>
-                <mode ival=\""++ModeNumber++"\"/>
-                <colfac fval=\"~.3f\"/>
-                <valfac fval=\"~.3f\"/>
-                </list_element>
-                <list_element>
-                <element sval=\"shader_node\"/>
-                <name sval=\"~s_mod\"/>
-                "++TexCo++"
-                <mapping sval=\"plain\"/>
-                <texture sval=\"~s\"/>
-                <type sval=\"texture_mapper\"/>
+            println(F, "\t\t<type sval=\"layer\"/>"),
 
-                <bump_strength fval=\"~.3f\"/>
-                </list_element>",
-                        [Texname,Texname,Diffuse,Shininess,Texname,Texname,Normal
-                        ])
+            % TODO: Atm, use IsColor value, but need review
+            println(F, "\t\t<use_alpha bval=\"~s\"/>",[IsColor]),
 
-            end
+            % close list element shader
+            println(F, "\t</list_element>"),
+            %%%-------------------------------->
+            % open list element for mapper
+            %
+            println(F, "\t<list_element>\n"
+                "\t\t<element sval=\"shader_node\"/>"),
+
+            % projection mapping coordinates type
+            MappingType = "plain", %proplists:get_value(projection, Ps, ?DEF_PROJECTION),
+
+            println(F, "\t\t<mapping sval=\"~s\"/>",[MappingType]),
+            println(F, "\t\t<name sval=\"~s_mod\"/>",[Texname]),
+            %
+            OffX = proplists:get_value(offsetx, Ps, 0.0),
+            OffY = proplists:get_value(offsety, Ps, 0.0),
+            OffZ = proplists:get_value(offsetz, Ps, 0.0),
+            %
+            println(F,
+                "\t\t<offset x=\"~w\" y=\"~w\" z=\"~w\"/>",
+                [OffX, OffY, OffZ]),
+            %
+            println(F,
+                "\t\t<proj_x ival=\"1\"/>\n"
+                "\t\t<proj_y ival=\"2\"/>\n"
+                "\t\t<proj_z ival=\"3\"/>"),
+
+            % for scale texture mapping
+            SizeX = proplists:get_value(size_x, Ps, 1.0),
+            SizeY = proplists:get_value(size_y, Ps, 1.0),
+            SizeZ = proplists:get_value(size_z, Ps, 1.0),
+            %
+            println(F, "\t\t<scale x=\"~w\" y=\"~w\" z=\"~w\"/>",
+                    [SizeX, SizeY, SizeZ]),
+
+            % texture coordinates type
+            % povman comment: Coordinates = proplists:get_value(coordinates, Ps, ?DEF_COORDINATES),
+            %
+            println(F, "\t\t<texco sval=\"~s\"/>",[TexCo]), %Coordinates]),
+            println(F, "\t\t<texture sval=\"~s\"/>",[Texname]),
+            println(F, "\t\t<type sval=\"texture_mapper\"/>"),
+            % if bumpmap..
+            case ShaderType of
+                bump ->
+                    println(F, "\t\t<bump_strength fval=\"~w\"/>",[Factor*5]);
+                _ -> ok
+            end,
+            % close mapper element
+            println(F, "\t</list_element>")
+
+            %end
 
     end.
