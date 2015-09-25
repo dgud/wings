ddce93dd12e10957e3b3bfc0fbd1d7400826cb27
povmaniaco <thebountyrenderer@gmail.com>
2015-09-25 17:45:46 +0200

    Indent and reordered some code lines, for improve readability.

--- plugins_src/import_export/bounty/exp_texture.erl
+++ plugins_src/import_export/bounty/exp_texture.erl
@@ -1,8 +1,7 @@
 %
 %
 %
-%
-%%%
+
 
 export_texture(F, Name, Maps, ExportDir, {modulator,Ps}) when is_list(Ps) ->
     case mod_enabled_mode_type(Ps, Maps) of
@@ -28,137 +27,103 @@ export_texture(F, Name, Maps, ExportDir, {modulator,Ps}) when is_list(Ps) ->
             export_texture(F, Name, Type, Ps)
     end.
 
-export_texture(F, Name, image, Filename) ->
-    println(F, "<texture name=\"~s\">~n"++
-                "    <filename sval=\"~s\"/>~n"++
-                "<type sval=\"image\"/>~n" ++
-                "</texture>", [Name,Filename]);
+export_texture(F, TexName, image, Filename) ->
+    println(F, "<texture name=\"~s\">",[TexName]),
+    println(F, "\t<filename sval=\"~s\"/>",[Filename]),
+    println(F, "\t<type sval=\"image\"/>"),
+    println(F, "\t</texture>");
+
 export_texture(F, Name, Type, Ps) ->
-
-%%% Start Work-Around for YafaRay Texture Name TEmytex Requirement for Noise Volume
-
-    TextureNameChg = re:replace(Name,"w_TEmytex_1","TEmytex",[global]),
-    println(F, "<texture name=\"~s\"> <type sval=\"~s\"/>", [TextureNameChg,format(Type)]),
-
-%%% End Work-Around for YafaRay Texture Name TEmytex Requirement for Noise Volume
-
-    Color1 = proplists:get_value(color1, Ps, ?DEF_MOD_COLOR1),
-    Color2 = proplists:get_value(color2, Ps, ?DEF_MOD_COLOR2),
-    Hard = proplists:get_value(hard, Ps, ?DEF_MOD_HARD),
-    NoiseBasis = proplists:get_value(noise_basis, Ps, ?DEF_MOD_NOISEBASIS),
-    NoiseSize = proplists:get_value(noise_size, Ps, ?DEF_MOD_NOISESIZE),
-    export_rgb(F, color1, Color1),
-    export_rgb(F, color2, Color2),
-    println(F, "    <hard bval=\"~s\"/>" " <noise_type sval=\"~s\"/>"  " <size fval=\"~.6f\"/>", [format(Hard),NoiseBasis,NoiseSize]),
+    %
+    %TextureNameChg = re:replace(Name,"w_TEmytex_1","TEmytex",[global]),
+    println(F, "<texture name=\"~s\">",[Name]),
+    println(F, "\t<type sval=\"~s\"/>", [format(Type)]),
+    %
+    export_rgb(F,
+        color1, proplists:get_value(color1, Ps, ?DEF_MOD_COLOR1)),
+    export_rgb(F,
+        color2, proplists:get_value(color2, Ps, ?DEF_MOD_COLOR2)),
+    println(F,
+        "\t<hard bval=\"~s\"/>",[format(proplists:get_value(hard, Ps, ?DEF_MOD_HARD))]),
+    println(F,
+        "\t<noise_type sval=\"~s\"/>",[proplists:get_value(noise_basis, Ps, ?DEF_MOD_NOISEBASIS)]),
+    println(F,
+        "\t<size fval=\"~.6f\"/>",[proplists:get_value(noise_size, Ps, ?DEF_MOD_NOISESIZE)]),
 
     case Type of
 
         clouds ->
-            Depth = proplists:get_value(depth, Ps, ?DEF_MOD_DEPTH),
-            println(F, "  <depth ival=\"~w\"/>" , [Depth]);
+            println(F, "\t<depth ival=\"~w\"/>", [proplists:get_value(depth, Ps, ?DEF_MOD_DEPTH)]);
 
         marble ->
-            Depth = proplists:get_value(depth, Ps, ?DEF_MOD_DEPTH),
+            println(F,
+                "\t<depth ival=\"~w\"/>",[proplists:get_value(depth, Ps, ?DEF_MOD_DEPTH)]),
+            println(F,
+                "\t<turbulence fval=\"~.6f\"/>",[proplists:get_value(turbulence, Ps, ?DEF_MOD_TURBULENCE)]),
+            println(F,
+                "\t<sharpness fval=\"~.6f\"/>",[proplists:get_value(sharpness, Ps, ?DEF_MOD_SHARPNESS)]),
+            println(F,
+                "\t<shape sval=\"~s\"/>",[proplists:get_value(shape, Ps, ?DEF_MOD_SHAPE)]);
 
-            Turbulence = proplists:get_value(turbulence, Ps,
-                                             ?DEF_MOD_TURBULENCE),
-
-            Sharpness = proplists:get_value(sharpness, Ps, ?DEF_MOD_SHARPNESS),
-
-            Shape = proplists:get_value(shape, Ps, ?DEF_MOD_SHAPE),
-
-            println(F, "  <depth ival=\"~w\"/>" "      <turbulence fval=\"~.6f\"/>~n" "  <sharpness fval=\"~.6f\"/>" " <shape sval=\"~s\"/>", [Depth,Turbulence,Sharpness,Shape]);
         wood ->
-            WoodType = proplists:get_value(wood_type, Ps,
-                                           ?DEF_MOD_WOODTYPE),
-
-
-            Turbulence = proplists:get_value(turbulence, Ps,
-                                             ?DEF_MOD_TURBULENCE),
-
-            Shape = proplists:get_value(shape, Ps, ?DEF_MOD_SHAPE),
-
             %% Coordinate rotation, see export_pos/3.
-            println(F, " <wood_type sval=\"~s\"/>"++
-                        "    <turbulence fval=\"~.6f\"/>~n" " <shape sval=\"~s\"/>",
-                    [WoodType,Turbulence,Shape]);
+            println(F,
+                "\t<wood_type sval=\"~s\"/>",[proplists:get_value(wood_type, Ps, ?DEF_MOD_WOODTYPE)]),
+            println(F,
+                "\t<turbulence fval=\"~.6f\"/>",[proplists:get_value(turbulence, Ps, ?DEF_MOD_TURBULENCE)]),
+            println(F,
+                "\t<shape sval=\"~s\"/>",[proplists:get_value(shape, Ps, ?DEF_MOD_SHAPE)]);
 
         voronoi ->
-            CellType = proplists:get_value(cell_type, Ps, ?DEF_MOD_CELLTYPE),
-
-            CellShape = proplists:get_value(cell_shape, Ps, ?DEF_MOD_CELLSHAPE),
-
-            CellSize = proplists:get_value(cell_size, Ps, ?DEF_MOD_CELLSIZE),
-
-            Intensity = proplists:get_value(intensity, Ps,  ?DEF_MOD_INTENSITY),
-
-            CellWeight1 = proplists:get_value(cell_weight1, Ps, ?DEF_MOD_CELL_WEIGHT1),
-
-            CellWeight2 = proplists:get_value(cell_weight2, Ps, ?DEF_MOD_CELL_WEIGHT2),
-
-            CellWeight3 = proplists:get_value(cell_weight3, Ps, ?DEF_MOD_CELL_WEIGHT3),
-
-            CellWeight4 = proplists:get_value(cell_weight4, Ps, ?DEF_MOD_CELL_WEIGHT4),
-
             %% Coordinate rotation, see export_pos/3.
-            println(F, " <color_type sval=\"~s\"/>"++
-                        "   <distance_metric sval=\"~s\"/>~n"
-                    "    <size fval=\"~.6f\"/>"++
-                        "    <intensity fval=\"~.6f\"/>~n"
-                    "    <weight1 fval=\"~.6f\"/>"++
-                        "    <weight2 fval=\"~.6f\"/>~n"
-                    "    <weight3 fval=\"~.6f\"/>"++
-                        "    <weight4 fval=\"~.6f\"/>",
-                    [CellType,CellShape,CellSize,Intensity,CellWeight1,CellWeight2,CellWeight3,CellWeight4]);
+            println(F,
+                "\t<color_type sval=\"~s\"/>",[proplists:get_value(cell_type, Ps, ?DEF_MOD_CELLTYPE)]),
+            println(F,
+                "\t<distance_metric sval=\"~s\"/>",[proplists:get_value(cell_shape, Ps, ?DEF_MOD_CELLSHAPE)]),
+            println(F,
+                "\t<size fval=\"~.6f\"/>",[proplists:get_value(cell_size, Ps, ?DEF_MOD_CELLSIZE)]),
+            println(F,
+                "\t<intensity fval=\"~.6f\"/>",[proplists:get_value(intensity, Ps,  ?DEF_MOD_INTENSITY)]),
+            println(F,
+                "\t<weight1 fval=\"~.6f\"/>",[proplists:get_value(cell_weight1, Ps, ?DEF_MOD_CELL_WEIGHT1)]),
+            println(F,
+                "\t<weight2 fval=\"~.6f\"/>",[proplists:get_value(cell_weight2, Ps, ?DEF_MOD_CELL_WEIGHT2)]),
+            println(F,
+                "\t<weight3 fval=\"~.6f\"/>",[proplists:get_value(cell_weight3, Ps, ?DEF_MOD_CELL_WEIGHT3)]),
+            println(F,
+                "\t<weight4 fval=\"~.6f\"/>",[proplists:get_value(cell_weight4, Ps, ?DEF_MOD_CELL_WEIGHT4)]);
 
         musgrave ->
-            MusgraveType = proplists:get_value(musgrave_type, Ps,
-                                               ?DEF_MOD_MUSGRAVE_TYPE),
-
+            MusgraveType = proplists:get_value(musgrave_type, Ps, ?DEF_MOD_MUSGRAVE_TYPE),
             NoiseBasis = proplists:get_value(noise_basis, Ps, ?DEF_MOD_NOISEBASIS),
-
             MusgraveNoiseSize = proplists:get_value(musgrave_noisesize, Ps, ?DEF_MOD_MUSGRAVE_NOISESIZE),
-
             MusgraveIntensity = proplists:get_value(musgrave_intensity, Ps, ?DEF_MOD_MUSGRAVE_INTENSITY),
-
             MusgraveContrast = proplists:get_value(musgrave_contrast, Ps, ?DEF_MOD_MUSGRAVE_CONTRAST),
-
             MusgraveLacunarity = proplists:get_value(musgrave_lacunarity, Ps, ?DEF_MOD_MUSGRAVE_LACUNARITY),
-
             MusgraveOctaves = proplists:get_value(musgrave_octaves, Ps, ?DEF_MOD_MUSGRAVE_OCTAVES),
 
             %% Coordinate rotation, see export_pos/3.
-            println(F, " <musgrave_type sval=\"~s\"/>"++
-                        " <noise_type sval=\"~s\"/>~n"
-                    "    <size fval=\"~.6f\"/>"++
-                        "    <intensity fval=\"~.6f\"/>~n"
-                    "    <H fval=\"~.6f\"/>"++
-                        "    <lacunarity fval=\"~.6f\"/>~n"
-                    "    <octaves fval=\"~.6f\"/>",
+            println(F, "\t<musgrave_type sval=\"~s\"/>\n"
+                       "\t<noise_type sval=\"~s\"/>~n"
+                       "\t<size fval=\"~.6f\"/>\n"
+                       "\t<intensity fval=\"~.6f\"/>~n"
+                       "\t<H fval=\"~.6f\"/>\n"
+                       "\t<lacunarity fval=\"~.6f\"/>~n"
+                       "\t<octaves fval=\"~.6f\"/>",
                     [MusgraveType,NoiseBasis,MusgraveNoiseSize,
                      MusgraveIntensity,MusgraveContrast,MusgraveLacunarity,MusgraveOctaves]);
 
         distorted_noise ->
 
-            NoiseBasis = proplists:get_value(noise_basis, Ps, ?DEF_MOD_NOISEBASIS),
-
-            DistortionType = proplists:get_value(distortion_type, Ps,
-                                                 ?DEF_MOD_DISTORTION_TYPE),
-
-            DistortionNoiseSize = proplists:get_value(distortion_noisesize, Ps, ?DEF_MOD_DISTORTION_NOISESIZE),
-
-            DistortionIntensity = proplists:get_value(distortion_intensity, Ps, ?DEF_MOD_DISTORTION_INTENSITY),
-
-
             %% Coordinate rotation, see export_pos/3.
-            println(F, " <noise_type1 sval=\"~s\"/>"++
-                        " <noise_type2 sval=\"~s\"/>~n"
-                    "    <size fval=\"~.6f\"/>"++
-                        "    <distort fval=\"~.6f\"/>~n",
-                    [NoiseBasis,DistortionType,DistortionNoiseSize,
-                     DistortionIntensity]);
-
-
+            println(F,
+                "\t<noise_type1 sval=\"~s\"/>",[proplists:get_value(noise_basis, Ps, ?DEF_MOD_NOISEBASIS)]),
+            println(F,
+                "\t<noise_type2 sval=\"~s\"/>",[proplists:get_value(distortion_type, Ps, ?DEF_MOD_DISTORTION_TYPE)]),
+            println(F,
+                "\t<size fval=\"~.6f\"/>",[proplists:get_value(distortion_noisesize, Ps, ?DEF_MOD_DISTORTION_NOISESIZE)]),
+            println(F,
+                "\t<distort fval=\"~.6f\"/>",[proplists:get_value(distortion_intensity, Ps, ?DEF_MOD_DISTORTION_INTENSITY)]);
         _ ->
             ok
     end,
