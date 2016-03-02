82efa1e6e28094d0942544d9a7c467ff498f059c
Dan Gudmundsson <dgud@users.sourceforge.net>
2016-03-02 15:43:24 +0100

    Convert view and selection group window to wx
    Changed so that all groups is always shown now regardless of selection mode.
    Automagically jumps to the first item of the selmode type when selection
    mode is changed.

--- plugins_src/commands/wpc_views_win.erl
+++ plugins_src/commands/wpc_views_win.erl
@@ -15,24 +15,17 @@
 -export([init/0,menu/2,command/2,win_data/1]).
 -export([window/1,window/5]).
 
--define(NEED_ESDL, 1).
--define(NEED_OPENGL, 1).
+-export([init/1,
+	 handle_call/3, handle_cast/2, handle_event/2, handle_info/2,
+	 code_change/3, terminate/2
+	]).
+
 -define(WIN_NAME, {plugin,saved_views}).
--include("wings.hrl").
+-include_lib("wings/src/wings.hrl").
 
 %%%
 %%% Saved Views window.
 %%%
--record(ost,
-	{st,					%Current St.
-	 n,					    %Number of objects.
-	 first,					%First object to show.
-	 os,					%All objects.
-	 active,				%Number of active object.
-	 tracking,				%Number of mouse tracked object.
-	 lh					    %Line height.
-	}).
-
 init() -> true.
 
 menu({window}, Menu) ->
@@ -43,8 +36,8 @@ menu({view}, Menu) ->
 		   (Entry) -> Entry
 		end,
     [PatchMenu(Entry) || Entry <- Menu];
-menu(_,Menu) -> 
-	Menu.
+menu(_,Menu) ->
+    Menu.
 
 camera_menu() ->
 	 {?__(1,"Manage Saved Views"), saved_views,
@@ -64,7 +57,7 @@ command(_,_) ->
 %% horiz alignment should be either "left" or "right"
 %% custom data is used to store windows properties and custom data - it should be parsed in window/5
 win_data(?WIN_NAME=Name) ->
-    {Name, {right,[{rollup, wings_wm:win_rollup(Name)}]}}.
+    {Name, {right,[]}}.
 
 window(St) ->
     case wings_wm:is_window(?WIN_NAME) of
@@ -80,35 +73,45 @@ window(St) ->
 	    keep
     end.
 
-window(?WIN_NAME, Pos, Size, CtmData, St) ->
-	Ps = CtmData,
-    Ost = #ost{first=0,lh=18,n=0,active=-1,tracking=-1},
-    Current = {current_state,St},
-    Op = {seq,push,event(Current, Ost)},
-    Props = [{display_data,geom_display_lists}],
-    wings_wm:toplevel(?WIN_NAME, title(), Pos, Size,
-		      [{sizeable,?PANE_COLOR},closable,vscroller,{anchor,ne},
-		       {properties,Props}|Ps], Op).
+window(WinName, Pos0, Size, Ps, St) ->
+    Parent = ?GET(top_frame),
+    View = get_view_state(St),
+    Pos = wxWindow:clientToScreen(Parent, Pos0),
+    Window = wx_object:start_link(?MODULE, [Parent, Pos, Size, Ps, View], []),
+    wings_wm:new(WinName, Window, {push,change_state(Window, St)}),
+    wings_wm:set_dd(WinName, geom_display_lists),
+    wings_frame:register_win(Window),
+    keep.
 
-get_event(Ost) ->
-    {replace,fun(Ev) -> event(Ev, Ost) end}.
+title() ->
+    ?__(1,"Saved views").
 
-event(redraw, Ost) ->
-    wings_io:ortho_setup(),
-    {W,H} = wings_wm:win_size(),
-    case wings_pref:get_value(bitmap_icons) of
-        false -> wings_io:border(0, 0, W-1, H-1, ?PANE_COLOR);
-        true ->
-          wings_io:blend(wings_pref:get_value(outliner_geograph_bg),
-            fun(Color) ->
-              wings_io:border(0, 0, W-1, H-1, Color)
-            end)
+%%%%%%%% View Window internals %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Inside wings (process)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+change_state(Window, St) ->
+    fun(Ev) -> forward_event(Ev, Window, St) end.
+
+forward_event(redraw, _Window, _St) -> keep;
+forward_event({current_state, St}, Window, St0) ->
+    case (New = get_view_state(St)) =:= get_view_state(St0) of
+	true  -> ignore;
+	false -> wx_object:cast(Window, {new_state, New})
     end,
-    draw_objects(Ost),
-    keep;
-event({action,{saved_views,Cmd}}, #ost{st=#st{views={_,Views0}}=St0}=Ost) ->
+    {replace, change_state(Window, St)};
+forward_event({apply, ReturnSt, Fun}, Window, St0) ->
+    %% Apply ops from window in wings process
+    case ReturnSt of
+	true ->
+	    St = Fun(St0),
+	    {replace, change_state(Window, St)};
+	false ->
+	    Fun(St0)
+    end;
+forward_event({action,{sel_groups,Cmd}}, _Window, #st{views={_,Views0}}=St0) ->
     case Cmd of
-        {save,Geom} ->
+        {save,Geom} -> %% New
             wings_wm:send(Geom, {action, {view, {views, {save,true}}}});
         {rename,_} ->
             wings_wm:send(geom, {action, {view, {views, rename}}});
@@ -122,247 +125,226 @@ event({action,{saved_views,Cmd}}, #ost{st=#st{views={_,Views0}}=St0}=Ost) ->
         {delete_all,_} ->
             wings_wm:send(geom, {action, {view, {views, delete_all}}})
     end,
-    get_event(Ost);
-event(resized, Ost) ->
-    update_scroller(Ost),
     keep;
-event(close, _) ->
-    delete;
-event(lost_focus, Ost) ->
-    get_event(Ost#ost{tracking=-1});
-event(got_focus, _) ->
-    Msg = wings_msg:button_format(?__(1,"Select"), [],
-				  ?__(2,"Shows saved views menu")),
-    wings_wm:message(Msg),
+forward_event({cursor, Cursor}, _, _) ->
+    wings_io:set_cursor(Cursor),
     keep;
-event({current_state,St}, Ost0) ->
-    Ost = update_state(St, Ost0),
-    update_scroller(Ost),
-    get_event(Ost);
-event(#mousemotion{y=Y}, #ost{}=Ost) ->
-    wings_wm:dirty(),
-    get_event(Ost#ost{tracking=active_object(Y,Ost)});
-event(#mousebutton{button=1,y=Y,state=?SDL_PRESSED}, #ost{active=Act}=Ost)
-  when Act >= 0 ->
-    case active_object(Y, Ost) of
-	Act ->
-	    wings_wm:grab_focus(),
-	    get_event(Ost#ost{tracking=-1});
+forward_event(Ev, Window, _) ->
+    wx_object:cast(Window, Ev),
+    keep.
+
+get_view_state(#st{views=Views}) -> Views.
+
+%%%%%%%% View Window internals %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Inside window process
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-record(state, {lc, views, drag}).
+
+init([Parent, Pos, Size, _Ps, VS]) ->
+    Frame = wings_frame:make_external_win(Parent, title(), [{size, Size}, {pos, Pos}]),
+    Panel = wxPanel:new(Frame),
+    Szr = wxBoxSizer:new(?wxVERTICAL),
+    Style = ?wxLC_REPORT bor ?wxLC_NO_HEADER bor ?wxLC_EDIT_LABELS,
+    LC = wxListCtrl:new(Panel, [{style, Style}]),
+    wxSizer:add(Szr, LC, [{proportion,1}, {flag, ?wxEXPAND}]),
+    wxListCtrl:insertColumn(LC, 0, "", [{width, ?wxLIST_AUTOSIZE_USEHEADER}]),
+    wxPanel:setSizer(Panel, Szr),
+    update_views(VS, undefined, LC),
+    Self = self(),
+    IgnoreForPopup = fun(Ev,_) ->
+			     case wx_misc:getMouseState() of
+				 #wxMouseState{rightDown=true} -> ignore;
+				 _ -> Self ! Ev
+			     end
+		     end,
+    wxWindow:connect(LC, command_list_item_selected, [{callback, IgnoreForPopup}]),
+    wxWindow:connect(LC, command_list_item_activated),
+    case os:type() of
+	{win32,nt} ->
+	    %% list_item_right_click does not work outside of items
+	    %% on windows use this instead
+	    wxWindow:connect(LC, command_right_click);
 	_ ->
+	    wxWindow:connect(LC, command_list_item_right_click),
 	    ok
     end,
-    get_event(Ost);
-event(#mousebutton{button=1,x=X,y=Y,state=?SDL_RELEASED}, #ost{}=Ost) ->
-    wings_wm:release_focus(),
-    case active_object(Y, Ost) of
-	Act when Act =/= -1 ->
-	    {X0,Y0} = wings_wm:local2global(X, Y),
-	    wings_wm:send(geom_focused(X0,Y0), {action, {view, {views, {jump,Act+1}}}}),
-	    wings_wm:dirty(),
-	    get_event(Ost#ost{active=Act});
-	_ -> keep
+    wxWindow:connect(LC, command_list_end_label_edit),
+    wxWindow:connect(LC, command_list_begin_drag),
+    wxWindow:connect(LC, left_up, [{skip, true}]),
+    wxWindow:connect(LC, size, [{skip, true}]),
+    wxWindow:show(Frame),
+    {Panel, #state{lc=LC, views=VS}}.
+
+handle_event(#wx{event=#wxList{type=command_list_end_label_edit, itemIndex=Indx}},
+	     #state{views={Curr, Views}, lc=LC} = State) ->
+    NewName = wxListCtrl:getItemText(LC, Indx),
+    case element(Indx+1, Views) of
+	{_, NewName} -> ignore;
+	{ViewInfo,_} ->
+	    Rename = fun(#st{}=St0) ->
+			     St = St0#st{views={Curr, setelement(Indx+1, Views, {ViewInfo,NewName})}},
+			     wings_wm:send(geom, {new_state,St}),
+			     St0 %% Intentional so we get updates to window process
+		     end,
+	    wings_wm:psend(?WIN_NAME, {apply, true, Rename})
+    end,
+    {noreply, State};
+
+handle_event(#wx{event=#wxSize{size={Width,_}}}, #state{lc=LC}=State) ->
+    wxListCtrl:setColumnWidth(LC, 0, Width-20),
+    {noreply, State};
+
+handle_event(#wx{event=#wxList{type=command_list_item_activated, itemIndex=Indx}},
+	     #state{views={_, Vs}}=State) ->
+    Indx >= 0 andalso wings_wm:psend(geom_focused(), {action, {view, {views, {jump,Indx+1}}}}),
+    {noreply, State#state{views={Indx+1, Vs}}};
+
+handle_event(#wx{event=#wxList{type=command_list_item_selected, itemIndex=Indx}},
+	     #state{views={_, Vs}}=State) ->
+    Indx >= 0 andalso wings_wm:psend(geom_focused(), {action, {view, {views, {jump,Indx+1}}}}),
+    {noreply, State#state{views={Indx+1, Vs}}};
+
+handle_event(#wx{event=#wxCommand{type=command_right_click}}, State) ->
+    invoke_menu(State),
+    {noreply, State};
+handle_event(#wx{event=#wxList{type=command_list_item_right_click}}, State) ->
+    invoke_menu(State),
+    {noreply, State};
+
+handle_event(#wx{event=#wxList{type=command_list_begin_drag, itemIndex=Indx}}, #state{lc=LC}=State) ->
+    wxWindow:setCursor(?GET(top_frame), wxCursor:new(?wxCURSOR_HAND)),
+    wxListCtrl:captureMouse(LC),
+    {noreply, State#state{drag=Indx}};
+handle_event(#wx{event=#wxMouse{type=left_up, x=X,y=Y}},
+	     #state{drag=Drag, views={_, Vs0}, lc=LC}=State) ->
+    case Drag of
+	undefined ->
+	    {noreply, State};
+	Drag ->
+	    Pos = {X,Y},
+	    wxListCtrl:releaseMouse(LC),
+	    wxWindow:setCursor(?GET(top_frame), ?wxNullCursor),
+	    case handle_drop(wxListCtrl:hitTest(LC,Pos, 0), Drag, Pos, Vs0, LC) of
+		false ->
+		    {noreply, State#state{drag=undefined}};
+		Vs ->
+		    Reorder = fun(#st{}=St0) ->
+				      St = St0#st{views=Vs},
+				      wings_wm:send(geom, {new_state,St}),
+				      St0 %% Intentional so we get updates to window process
+			      end,
+		    wings_wm:psend(?WIN_NAME, {apply, true, Reorder}),
+		    {noreply, State#state{drag=undefined}}
+	    end
     end;
-event(#mousebutton{button=4,state=?SDL_RELEASED}, Ost) ->
-    zoom_step(-1*lines(Ost) div 4, Ost);
-event(#mousebutton{button=5,state=?SDL_RELEASED}, Ost) ->
-    zoom_step(lines(Ost) div 4, Ost);
-event(#mousebutton{}=Ev, Ost) ->
-    case wings_menu:is_popup_event(Ev) of
-	no -> keep;
-	{yes,X,Y,_} -> do_menu(X, Y, Ost)
-    end;
-event(scroll_page_up, Ost) ->
-    zoom_step(-lines(Ost), Ost);
-event(scroll_page_down, Ost) ->
-    zoom_step(lines(Ost), Ost);
-event(Ev, Ost) ->
-    case wings_hotkey:event(Ev) of
-	{_,_}=Act ->
-	    wings_wm:send(geom, {action,Act}),
-	    wings_wm:dirty(),
-	    get_event(Ost);
-	_ -> keep
+
+handle_event(#wx{} = Ev, State) ->
+    io:format("~p:~p Got unexpected event ~p~n", [?WIN_NAME,?LINE, Ev]),
+    {noreply, State}.
+
+%%%%%%%%%%%%%%%%%%%%%%
+
+handle_call(_Req, _From, State) ->
+    %% io:format("~p:~p Got unexpected call ~p~n", [?WIN_NAME,?LINE, _Req]),
+    {reply, ok, State}.
+
+handle_cast({new_state, Views}, #state{lc=LC, views=Old}=State) ->
+    update_views(Views, Old, LC),
+    {noreply, State#state{views=Views}};
+handle_cast(_Req, State) ->
+    io:format("~p:~p Got unexpected cast ~p~n", [?WIN_NAME,?LINE, _Req]),
+    {noreply, State}.
+
+handle_info(Msg, State) ->
+    io:format("~p:~p Got unexpected info ~p~n", [?WIN_NAME,?LINE, Msg]),
+    {noreply, State}.
+
+%%%%%%%%%%%%%%%%%%%%%%
+
+code_change(_From, _To, State) ->
+    State.
+
+terminate(_Reason, _) ->
+    io:format("terminate: ~p (~p)~n",[?WIN_NAME, _Reason]),
+    wings ! {external, fun(_) -> wings_wm:delete(?WIN_NAME) end},
+    normal.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+
+update_views(Old, Old, _LC) -> Old;
+update_views({Indx, Tuple}=New, _Old, LC) ->
+    Add = fun({_View,Name}, Id) ->
+		  wxListCtrl:insertItem(LC, Id, Name),
+		  Id+1
+	  end,
+    wxListCtrl:deleteAllItems(LC),
+    wx:foldl(Add, 0, tuple_to_list(Tuple)),
+    wxListCtrl:setItemState(LC, Indx-1, 16#FFFF, ?wxLIST_STATE_SELECTED),
+    New.
+
+get_selection(LC) ->
+    case wxListCtrl:getSelectedItemCount(LC) of
+	0 -> none;
+	1 ->
+	    Opts = [{geometry,?wxLIST_NEXT_ALL}, {state, ?wxLIST_STATE_SELECTED}],
+	    wxListCtrl:getNextItem(LC, -1, Opts)
     end.
 
-%% there are no views
-do_menu(X, Y ,#ost{st=#st{views={_,{}}}}=_Ost) ->
-    Menu=views_menu({1,{new,geom_focused(X,Y)}}),
-    wings_menu:popup_menu(X, Y, saved_views, Menu);
-do_menu(X, Y, #ost{active=Act,st=#st{views={_,Views}},tracking=Trk}=_Ost) ->
-    Objs = objs_from_view(Views),
-    Id = act_to_key(Objs,Act),
-    Menu0 = views_menu({length(Objs)+1,{new,geom_focused(X,Y)}}),  % Save option
-    Menu1 = case Trk of
-        Act -> Menu0++views_menu(Id);  % Rename and Delete option
-        _ -> Menu0
-    end,
-    Menu2 = Menu1++views_menu({none,delete}),  % Delete All option
-    Menu = if Trk=/=-1 ->
-        Id0 = act_to_key(Objs,Trk),
-        Menu2++
-        views_menu({Trk,Id0}); % Replace option
-      true ->
-        Menu2
-    end,
-	wings_menu:popup_menu(X, Y, saved_views, Menu).
+handle_drop(-1, Drop, {X,Y}=Pos, Vs, LC) ->
+    {W,_H} = wxListCtrl:getClientSize(LC),
+    if 0 > X -> false;
+       X > W -> false;
+       0 > Y -> handle_drop(0, Drop, Pos, Vs, LC);
+       true  -> handle_drop(tuple_size(Vs), Drop, Pos, Vs, LC)
+    end;
+handle_drop(Drop, Drop, _Pos, _Vs0, _LC) -> false;
+handle_drop(Hit, Drop, _Pos, Vs0, _LC) ->
+    DroppedItem = element(Drop+1, Vs0),
+    {Hit+1, list_to_tuple(insert_to_list(0, Hit, Drop, DroppedItem, tuple_to_list(Vs0)))}.
 
-views_menu({Idx, {new,Geom}}) ->
-	[{?__(1,"Save..."),menu_cmd(save,Geom), ?__(2,"Save this view at ") ++"["++integer_to_list(Idx)++"]"}];
-views_menu({_, delete}) ->
-	[separator,
-	 {?__(9,"Delete All..."), menu_cmd(delete_all,all), ?__(10,"Delete all saved views")}];
-views_menu({_, {Idx,Legend}}) ->
-	[{?__(3,"Replace "),menu_cmd(replace,Idx), ?__(4,"Replaces \"")++Legend++"\"["++integer_to_list(Idx)++"]\" settings with the current viewing ones"}];
+insert_to_list(Here, Here, Drop, Item, [Next|Vs]) ->
+    [Item,Next|insert_to_list(Here+1,Here,Drop,Item,Vs)];
+insert_to_list(Drop, Pos, Drop, Item, [_|Vs]) ->
+    insert_to_list(Drop+1, Pos, Drop, Item, Vs);
+insert_to_list(Indx, Here, Drop, Item, [This|Vs]) ->
+    [This|insert_to_list(Indx+1, Here, Drop, Item, Vs)];
+insert_to_list(Indx, Here, _Drop, Item, []) ->
+    case Here =:= Indx of
+	true -> [Item];
+	false -> []
+    end.
+
+invoke_menu(#state{views=Views, lc=LC}) ->
+    Menus = get_menus(get_selection(LC), Views),
+    Pos = wx_misc:getMousePosition(),
+    Cmd = fun(_) -> wings_menu:popup_menu(LC, Pos, sel_groups, Menus) end,
+    wings_wm:psend(?WIN_NAME, {apply, false, Cmd}).
+
+get_menus(_, {_, {}}) ->
+    views_menu({new,geom_focused()});
+get_menus(none, _) ->
+    views_menu({new,geom_focused()}) ++ [separator|views_menu(delete_all)];
+get_menus(Indx, {_,Views}) ->
+    {_,Legend} = element(Indx+1, Views),
+    views_menu({Indx+1, Legend}) ++ [separator|get_menus(none, undefined)].
+
+views_menu({new,Geom}) ->
+    [{?__(1,"Save New..."),menu_cmd(save,Geom), ?__(2,"Create a new saved view")}];
+views_menu(delete_all) ->
+    [{?__(9,"Delete All..."), menu_cmd(delete_all,all), ?__(10,"Delete all saved views")}];
 views_menu({Idx, Legend}) ->
-	[separator,
-	 {?__(5,"Rename..."), menu_cmd(rename,none), ?__(6,"Rename \"")++Legend++"\"["++integer_to_list(Idx)++"]\""},
-	 {?__(7,"Delete"), menu_cmd(delete,none), ?__(8,"Delete \"")++Legend++"\"["++integer_to_list(Idx)++"]\""}].
+    [{?__(3,"Replace "),menu_cmd(replace,Idx),
+      ?__(4,"Replaces \"")++Legend++"\"["++integer_to_list(Idx)++"]\" settings with the current viewing ones"},
+     {?__(5,"Rename..."), menu_cmd(rename,none),
+      ?__(6,"Rename \"")++Legend++"\"["++integer_to_list(Idx)++"]\""},
+     {?__(7,"Delete"), menu_cmd(delete,none),
+      ?__(8,"Delete \"")++Legend++"\"["++integer_to_list(Idx)++"]\""}].
 
 menu_cmd(Cmd, Id) ->
     {'VALUE',{Cmd,Id}}.
 
-geom_focused(X, Y) ->
-    case wings_wm:geom_below(X, Y) of
-        none -> geom;
-        Geom -> Geom
-    end.
-
-%%%
-%%% Updating the state.
-%%%
-
-update_state(#st{views={_,Views}}=St, #ost{active=Act0,first=OldFirst}=Ost0) ->
-    Act=case Views of
-      {} -> -1;
-      _ -> Act0  % undefined
-    end,
-    #ost{first=First0} = Ost = update_state_1(St, Ost0#ost{active=Act}),
-    case clamp(First0, Ost) of
-	  OldFirst -> Ost;
-	  First ->
-	    wings_wm:dirty(),
-	    Ost#ost{first=First}
-    end.
-
-update_state_1(St, Ost) ->
-    update_state_2(St, Ost).
-
-update_state_2(#st{views={CurrentView,Views}}=St, #ost{os=Objs0}=Ost) ->
-    Objs = objs_from_view(Views),
-    case Objs of
-	Objs0 -> ok;
-	_ -> wings_wm:dirty()
-    end,
-    N = length(Objs),
-    Act = if CurrentView > 0 -> CurrentView-1;
-      true -> CurrentView
-    end,
-    Ost#ost{st=St,os=Objs,n=N,active=Act}.
-
-update_scroller(#ost{n=0}) ->
-    Name = wings_wm:this(),
-    wings_wm:set_knob(Name, 0.0, 1.0);
-update_scroller(#ost{first=First,n=N}=Ost) ->
-    Name = wings_wm:this(),
-    Lines = lines(Ost),
-    wings_wm:set_knob(Name, First/N, Lines/N).
-
-objs_from_view(Views) ->
-    [{_,Vn}]=lists:foldl(fun({_,Legend}, [{Idx0,Acc}]) ->
-        Idx = Idx0+1,
-        [{Idx, Acc++[{Idx,Legend}]}]
-    end, [{0,[]}], tuple_to_list(Views)),
-    lists:flatten(Vn).
-
-act_to_key(_,-1) -> none;
-act_to_key(Objs,Act) ->
-    lists:nth(Act+1, Objs).
-
-zoom_step(Step, #ost{first=First0}=Ost0) ->
-    case clamp(First0+Step, Ost0) of
-	First0 -> keep;
-	First ->
-	    wings_wm:dirty(),
-	    Ost = Ost0#ost{first=First},
-	    update_scroller(Ost),
-	    get_event(Ost)
-    end.
-
-clamp(F, #ost{n=N}=Ost) ->
-    Max = case N-lines(Ost) of
-	      Neg when Neg < 0 -> 0;
-	      Other -> Other
-	  end,
-    if
-	F < 0 -> 0;
-	F > Max -> Max;
-	true -> F
-    end.
-    
-lines(#ost{lh=Lh}) ->
-    {_,_,_,H} = wings_wm:viewport(),
-    H div Lh.
-
-title() ->
-    ?__(1,"Saved views").
-
-active_object(Y0, #ost{lh=Lh,first=First,n=N}) ->
-    case Y0 - top_of_first_object() of
-	Y when Y < 0 -> -1;
-	Y1 ->
-	    case Y1 div Lh of
-		Y when First+Y < N ->
-		    First+Y;
-		_ -> -1
-	    end
-    end.
-
-draw_objects(#ost{os=Objs0,first=First,lh=Lh,active=Active,tracking=Trk,n=N0}=Ost) ->
-    {W,_H} = wings_wm:win_size(),
-    Objs = lists:nthtail(First, Objs0),
-    Lines = lines(Ost),
-    N = case N0-First of
-	    N1 when N1 < Lines -> N1;
-	    _ -> Lines
-	end,
-	Y=Lh-2,
-    draw_objects_1(N, Objs, Ost, W, Active-First, Y),
-    if
-      Trk=/=-1 ->
-        Y0=Y+(Trk*Lh),
-        draw_frame(2,Y0-?CHAR_HEIGHT,W-2,Y0+4);
-      true -> ok
-    end.
-
-draw_objects_1(0, _, _, _, _, _) -> ok;
-draw_objects_1(N, [{_,Name}|Objs], #ost{lh=Lh}=Ost, R, Active, Y) ->
-    B = wings_pref:get_value(bitmap_icons),
-    case Active =:= 0 of
-    true when B ->
-      gl:color3fv(wings_pref:get_value(outliner_geograph_hl)),
-      gl:recti(2, Y-?CHAR_HEIGHT, R-2, Y+4),
-      gl:color3fv(wings_pref:get_value(outliner_geograph_hl_text));
-    true ->
-      gl:color3f(0.0, 0.0, 0.5),
-      gl:recti(2, Y-?CHAR_HEIGHT, R-2, Y+4),
-      gl:color3f(1.0, 1.0, 1.0);
-    false -> ok
-    end,
-    wings_io:text_at(4, Y, Name),
-    gl:color3b(0, 0, 0),
-    draw_objects_1(N-1, Objs, Ost, R, Active-1, Y+Lh).
-
-draw_frame(X1, Y1, X2, Y2) ->
-    gl:'begin'(?GL_LINE_LOOP),
-    gl:vertex2i(X1+1, Y1+1),
-    gl:vertex2i(X2, Y1),
-    gl:vertex2i(X2, Y2),
-    gl:vertex2i(X1, Y2),
-    gl:vertex2i(X1+1, Y1), % force x1,y1 corner (dot) be drawn
-    gl:'end'().
-
-top_of_first_object() ->
-    0.
-
 current() ->
-    wings_wm:get_prop(geom, current_view).
+    wings_wm:get_prop(geom_focused(), current_view).
+
+geom_focused() -> geom.
