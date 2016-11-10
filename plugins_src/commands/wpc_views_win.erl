4be4b783561461b7efd4b3917cf0821e31c29172
micheus <micheus@gmail.com>
2016-11-10 13:13:54 -0200

    Fix the crash when disabling a plugin which has a window docked
    The issue was observed when disabling a plugin with window docked and after
    left and start/restating Wings3D we got a hard crash that denied Wings3d to
    be started. The way to fix that was only by editing the preferences.txt.

--- plugins_src/commands/wpc_views_win.erl
+++ plugins_src/commands/wpc_views_win.erl
@@ -12,7 +12,7 @@
 %%
 
 -module(wpc_views_win).
--export([init/0,menu/2,command/2,win_data/1]).
+-export([init/0,menu/2,command/2,win_data/1,win_name/0]).
 -export([window/1,window/5]).
 
 -export([init/1,
@@ -58,6 +58,9 @@ command(_,_) ->
 %% custom data is used to store windows properties and custom data - it should be parsed in window/5
 win_data(?WIN_NAME=Name) ->
     {Name, {right,[]}}.
+
+win_name() ->
+    ?WIN_NAME.
 
 window(St) ->
     case wings_wm:is_window(?WIN_NAME) of
