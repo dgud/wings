d1c748a3b37234317567ab3931a8d20716d865a8
Björn Gustavsson <bgustavsson@gmail.com>
2017-01-14 10:58:08 +0100

    wpc_autouv: Eliminate call to wings_shape

--- plugins_src/autouv/wpc_autouv.erl
+++ plugins_src/autouv/wpc_autouv.erl
@@ -1008,7 +1008,9 @@ fake_sel_1(St0) ->
 
 add_faces(NewFs,St0=#st{bb=ASt=#uvstate{id=Id,mode=Mode,st=GeomSt=#st{shapes=Shs0}}}) ->
     case {NewFs,Mode} of
-	{_,object} -> wings_shape:show_all(St0);
+	{_,object} ->
+            All = wings_obj:fold(fun(#{id:=I}, A) -> [I|A] end, [], St0),
+            wings_obj:unhide(All, St0);
 	{object,_} -> %% Force a chart rebuild, we are switching object mode
 	    We = gb_trees:get(Id,Shs0),
 	    Shs = gb_trees:update(Id, We#we{fs=undefined,es=array:new()}, Shs0),
