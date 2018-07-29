dgud/wings

commit b924c7ba96a6372530c8736c74dee1179b99ae4c

Fix bug in exporting and saving lights
The bug was introduced in bdb75f6c22e.

src/wings_light.erl
@@ -599,10 +599,11 @@ export_camera_lights() ->
 	 end,
     [GL(Light) || Light <- [Amb|Ls]].
 
-get_light(#{id:=Id,perm:=P,light:=Light}, BC, St) ->
+get_light(#{id:=Id,name:=Name,perm:=P,light:=Light}, BC, St) ->
     F = fun(We) -> get_light_1(Light, We, BC) end,
     Ps0 = wings_obj:with_we(F, Id, St),
-    export_perm(P, Ps0).
+    Ps = export_perm(P, Ps0),
+    {Name,Ps}.
 
 get_light(#we{name=Name,perm=P,light=Light}=We, BC) ->
     Ps0 = get_light_1(Light, We, BC),

commit d5a048ec1620bc1178e2976e0e6d8a7d01228582

wings_light: Fix bug introuced when fixing the previous bug
The bug was introduced in b924c7ba96a63.

src/wings_light.erl
@@ -603,7 +603,10 @@ get_light(#{id:=Id,name:=Name,perm:=P,light:=Light}, BC, St) ->
     F = fun(We) -> get_light_1(Light, We, BC) end,
     Ps0 = wings_obj:with_we(F, Id, St),
     Ps = export_perm(P, Ps0),
-    {Name,Ps}.
+    case BC of
+        true -> {Name,Ps};
+        false -> Ps
+    end.
 
 get_light(#we{name=Name,perm=P,light=Light}=We, BC) ->
     Ps0 = get_light_1(Light, We, BC),