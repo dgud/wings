dgud/wings

commit 4820c06961c96faca0d4dc95fced2dd17cb087f6

Add types for most elements in the #dlo{} record

src/wings.hrl
@@ -245,45 +245,55 @@
 	 yon					%Far clipping plane.
 	}).
 
-
 %% Display lists per object.
 %%  Important: Plain integers and integers in lists will be assumed to
 %%  be display lists. Arbitrary integers must be stored inside a tuple
 %%  or record to not be interpreted as a display list.
 -record(dlo,
-	{work=none,				%Workmode faces.
-	 smooth=none,				%Smooth-shaded faces.
-	 edges=none,				%Edges and wire-frame.
-	 vs=none,				%Unselected vertices.
-	 hard=none,				%Hard edges.
-	 sel=none,				%Selected items.
-	 orig_sel=none,				%Original selection.
-	 normals=none,				%Normals.
+        {work=none :: wings_dl:dl(),            %Workmode faces.
+         smooth=none :: wings_dl:dl(),          %Smooth-shaded faces.
+         edges=none :: wings_dl:dl(),           %Edges and wire-frame.
+         vs=none :: wings_dl:dl(),              %Unselected vertices.
+         hard=none :: wings_dl:dl(),            %Hard edges.
+         sel=none :: wings_dl:sel_dl(),         %Selected items.
+         orig_sel=none :: wings_dl:sel_dl(),    %Original selection.
+         normals=none :: wings_dl:dl(),         %Normals.
 
-	 vab     = none,                        %% Vertex array (see below)
-	 tri_map = none,			%Tri -> Face map (for picking)
+         vab=none :: 'none' | vab(),            %Vertex array (see below)
+         tri_map=none :: 'none' | wings_pick:tri_map(), %Tri -> Face map (for picking)
 
 	 %% Miscellanous.
-	 hilite=none,				%Hilite display list.
-	 mirror=none,				%Virtual mirror data.
-	 ns=none,				%Normals/positions per face.
+         hilite=none ::
+           'none' | {wings_sel:mode(),wings_dl:dl()},  %Hilite display list.
+         mirror=none ::
+           'none' | e3d_mat:matrix(),           %Virtual mirror data.
+         ns=none :: wings_draw:normals(),       %Normals/positions per face.
 	 plugins=[],                            %Draw lists for plugins.
-	 proxy=false,                           %Proxy Data is used.
+
+         %% Proxy stuff.
+         proxy=false :: boolean(),              %Proxy Data is used.
+         proxy_data=none ::
+           'none' | wings_proxy:sp(),           %Data for smooth proxy.
 
 	 %% Source for display lists.
-	 src_we=none,				%Source object.
-	 src_sel=none,				%Source selection.
-	 orig_mode=none,			%Original selection mode.
-	 split=none,				%Split data.
-	 drag=none,				%For dragging.
-	 transparent=false,			%Object includes transparancy.
-	 proxy_data=none,			%Data for smooth proxy.
-	 open=false,				%Open (has hole).
+         src_we=none :: 'none' | #we{},         %Source object.
+         src_sel=none :: wings_dl:dl()
+                       | {wings_sel:mode(),wings_sel:item_set()}, %Source selection.
+	 orig_mode=none ::
+           'none' | wings_sel:mode(), %Original selection mode.
+         split=none ::
+           'none' | wings_draw:split(),         %Split data.
+         drag=none :: 'none'
+                    | wings_drag:drag()
+                    | wings_tweak:drag()
+                    | {matrix,_,_,e3d_mat:matrix()}, %For dragging.
+         transparent=false :: boolean() | #we{}, %Object includes transparancy.
+         open=false :: boolean(),               %Open (has hole).
 
 	 %% List of display lists known to be needed only based
 	 %% on display modes, not whether the lists themselves exist.
 	 %% Example: [work,edges]
-	 needed=[]
+         needed=[] :: [atom()]
 	}).
 
 %% Vertex Buffer Objects. The name is #vab{} for historical reasons.
@@ -304,7 +314,10 @@
 	  face_vc  = none :: wings_vtx_buffer(), %Vertex Colors coords
 	  face_es  = none ::
             {0, binary()} | wings_vtx_buffer(),  %Edges 2*Vertex coords
-	  face_map = none,                       %FaceId -> {BinPos,TriCount}
+	  face_map = none ::
+            'none' | wings_draw_setup:face_map(), %FaceId -> {BinPos,TriCount}
 	  mat_map  = none                        %Face per Material draw info
 	 }).
+
+-type vab() :: #vab{}.
 

src/wings_dl.erl
@@ -19,6 +19,7 @@
 	 update/2,map/2,fold/2,changed_materials/1,
 	 display_lists/0,
 	 call/1,mirror_matrix/1,draw/3]).
+-export_type([dl/0,real_dl/0,sel_dl/0]).
 
 %%% This module manages Vertex Buffer Objects (VBOs, represented by
 %%% #vab{} records) for all objects in a Geometry or AutoUV window.
@@ -46,6 +47,16 @@
 -define(NEED_OPENGL, 1).
 -include("wings.hrl").
 -import(lists, [reverse/1,foreach/2,foldl/3]).
+
+-type vbo() :: {'vbo',non_neg_integer()}.
+-type draw_fun() :: fun(() -> 'ok').
+-type real_dl() :: draw_fun()
+                 | {'call',draw_fun()|'none',vab()|vbo()}
+                 | {'call_in_this_win',_,draw_fun()}
+                 | [real_dl()].
+-type dl() :: 'none' | real_dl().
+-type sel_dl() :: 'none' | vab() | vbo() | draw_fun()
+                | {'call',draw_fun()|'none',vab()|vbo()}.
 
 -record(du,
 	{dl=[],					%Display list records.
@@ -144,6 +155,8 @@ fold(Fun, Acc) ->
 %% call(Term)
 %%  Call OpenGL to render the geometry using the VBOs embedded in
 %%  the term.
+
+-spec call(dl()) -> 'ok'.
 
 call(none) -> none;
 call({call,Dl,_Vbo}) ->

src/wings_drag.erl
@@ -16,7 +16,7 @@
          matrix/3,matrix/4,general/4,drag_only/4,
          compose/1,translate_fun/2]).
 
--export_type([vec_transform_fun/0,vertices/0,vertex_transform/0]).
+-export_type([drag/0,vec_transform_fun/0,vertices/0,vertex_transform/0]).
 
 -define(NEED_ESDL, 1).
 -define(NEED_OPENGL, 1).
@@ -51,6 +51,7 @@
 	 last_move,		% Last move.
          drag                   % Drag fun.
 	}).
+
 
 %% Drag per object.
 
@@ -105,6 +106,9 @@
               | {'mode',{mode_fun(),mode_data()}}
               | {'rescale_normals',boolean()}
               | 'screen_relative'.
+
+-type drag() :: #drag{} | {'general',general_fun()} | #do{}.
+
 
 -spec fold(Fun, [unit()], #st{}) -> {'drag',#drag{}} when
       Fun :: fun((wings_sel:item_set(), #we{}) -> fold_tv()).

src/wings_draw.erl
@@ -21,6 +21,8 @@
 %% Export for plugins (and wings_proxy) that need to draw stuff
 -export([draw_flat_faces/2,draw_smooth_faces/2]).
 
+-export_type([normals/0,split/0]).
+
 -define(NEED_OPENGL, 1).
 -define(NEED_ESDL, 1).
 -include("wings.hrl").
@@ -37,6 +39,12 @@
 	 orig_we,
 	 orig_st		      %For materials
 	}).
+
+-type normals() :: 'none'
+                 | array:array(e3d_vec:vector())
+                 | {normals()}.
+
+-opaque split() :: #split{}.
 
 %%%
 %%% Refresh the display lists from the contents of St.

src/wings_draw_setup.erl
@@ -23,10 +23,16 @@
 -export([create_vab/4,create_tangent_vab/5,
 	 add_ts/5,add_tangents/3]).
 
+-export_type([face_map/0,face_tris/0]).
+
 -define(NEED_OPENGL, 1).
 -include("wings.hrl").
 
 -import(lists, [reverse/1,sort/1,foldl/3]).
+
+
+-type face_tris() :: {non_neg_integer(),non_neg_integer()}.
+-type face_map() :: array:array(face_tris()) | [face_tris()].
 
 %%%
 %%% we(We, [Option], St) -> #vab{} See wings.hrl

src/wings_pick.erl
@@ -16,6 +16,8 @@
 -export([do_pick/3,raw_pick/3]).
 -export([paint_pick/3]).
 
+-export_type([tri_map/0]).
+
 -define(NEED_OPENGL, 1).
 -define(NEED_ESDL, 1).
 -include("wings.hrl").
@@ -45,6 +47,8 @@
 	 filter,                                %Filter fun, allow hl? true/false
 	 prev=none				%Previous hit ({Id,Item}).
 	}).
+
+-type tri_map() :: [{wings_face:face_num(),wings_draw_setup:face_tris()}].
 
 event(Ev, St) ->
     event(Ev, St, St).

src/wings_proxy.erl
@@ -15,6 +15,7 @@
 -export([setup/1,quick_preview/1,update/2,draw_smooth_edges/2,
 	 smooth/2, smooth_dl/1, flat_dl/1, invalidate/2,
 	 split_proxy/3, update_dynamic/3, reset_dynamic/1]).
+-export_type([sp/0]).
 
 -define(NEED_OPENGL, 1).
 -include("wings.hrl").
@@ -41,6 +42,8 @@
 	 %% Note: face_map is a list ordered in face appearance order in bins above
 	 
 	}).
+
+-type sp() :: #sp{}.
 
 quick_preview(_St) ->
     case any_proxy() of

src/wings_tweak.erl
@@ -21,6 +21,8 @@
 
 -export([tweak_keys/0, menu/0, tweak_magnet_menu/0, constraints_menu/0]).  %% For wings_tweak_win only
 
+-export_type([drag/0]).
+
 -define(NEED_OPENGL, 1).
 -define(NEED_ESDL, 1).
 
@@ -62,6 +64,7 @@
 	 vs,        % [{V,Pos,Distance,Influence}]
 	 vtab=[]}). % [{V,Pos}] (latest)
 
+-type drag() :: #drag{}.
 
 %%%
 %%% Set Default Tweak prefs

commit dc3108c5744dcf1923351ab17a9820ebcab17329

wings.hrl: Fix over-specified type of #dlo.src_sel
src_sel never contains a display list. The bug was introduced
in 4820c06961c9.

src/wings.hrl
@@ -277,8 +277,8 @@
 
 	 %% Source for display lists.
          src_we=none :: 'none' | #we{},         %Source object.
-         src_sel=none :: wings_dl:dl()
-                       | {wings_sel:mode(),wings_sel:item_set()}, %Source selection.
+         src_sel=none :: 'none' |
+                         {wings_sel:mode(),wings_sel:item_set()}, %Source selection.
          split=none ::
            'none' | wings_draw:split(),         %Split data.
          drag=none :: 'none'