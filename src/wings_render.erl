dgud/wings

commit 123cc7ff6a956836c2dd79c5e264ce780577f021

Cleanup alpha handling in smooth-preview

src/wings_render.erl
@@ -139,7 +139,7 @@ setup_scene_lights(true, Lights, RS0) ->
 
 render_objects(Mode, PM, MM, UseSceneLights) ->
     Dls = wings_dl:display_lists(),
-    {Open,Closed,Lights} = split_objects(Dls, [], [], []),
+    {Transp, Open,Closed,Lights} = split_objects(Dls, false, [], [], []),
     NonLights = Open ++ Closed,
     RS0 = #{ws_eyepoint => e3d_mat:mul_point(e3d_transform:inv_matrix(MM), {0.0,0.0,0.0}),
             view_from_world => MM},
@@ -148,10 +148,10 @@ render_objects(Mode, PM, MM, UseSceneLights) ->
     case wings_wm:get_prop(workmode) of
 	false ->
             RS10 = case UseSceneLights of
-                       true -> render_smooth_objects(Open, Closed, ambient, RS2); %% amb pass
+                       true -> render_smooth_objects(Transp, Open, Closed, ambient, RS2); %% amb pass
                        false -> RS2
                    end,
-            RS21 = render_smooth_objects(Open, Closed, SL, RS10),
+            RS21 = render_smooth_objects(Transp, Open, Closed, SL, RS10),
             RS22 = render_wire(NonLights, Mode, true, RS21),
             render_sel_highlight(NonLights, Mode, true, PM, RS22);
 	true ->
@@ -216,7 +216,7 @@ render_work_objects_1([D|Dls], SceneLights, RS0) ->
     render_work_objects_1(Dls, SceneLights, RS);
 render_work_objects_1([], _, RS) -> RS.
 
-render_smooth_objects(Open, Closed, SceneLights, RS0) ->
+render_smooth_objects(Transp, Open, Closed, SceneLights, RS0) ->
     gl:shadeModel(?GL_SMOOTH),
     gl:polygonMode(?GL_FRONT_AND_BACK, ?GL_FILL),
     RS1 = enable_lighting(SceneLights, RS0),
@@ -226,30 +226,33 @@ render_smooth_objects(Open, Closed, SceneLights, RS0) ->
     RS2 = render_smooth_objects_0(Closed, false, SceneLights, RS1),
     wings_pref:get_value(show_backfaces) andalso gl:disable(?GL_CULL_FACE),
     RS3 = render_smooth_objects_0(Open, false, SceneLights, RS2),
-
-    %% Render a alpha test pass for almost opaque fragments
-    gl:disable(?GL_BLEND),
-    gl:drawBuffer(?GL_NONE),
-    polygonOffset(3.0),
-    RS4 = wings_shaders:use_prog(alpha_test, RS3),
-    RS5 = render_smooth_objects_0(Closed, true, false, RS4),
-    RS6 = render_smooth_objects_0(Open, true, false, RS5),
-    gl:drawBuffer(?GL_BACK),
-    gl:depthFunc(?GL_LEQUAL),
-    polygonOffset(2.0),
-    RS8 = enable_lighting(SceneLights, RS6),
-    gl:enable(?GL_BLEND),
-    gl:blendFunc(?GL_SRC_ALPHA, ?GL_ONE_MINUS_SRC_ALPHA),
-    gl:depthMask(?GL_FALSE),
-    gl:enable(?GL_CULL_FACE),
-    RS9 = render_smooth_objects_0(Closed, true, SceneLights, RS8),
-    wings_pref:get_value(show_backfaces) andalso gl:disable(?GL_CULL_FACE),
-    RS10 = render_smooth_objects_0(Open, true, SceneLights, RS9),
-    RS = disable_lighting(RS10),
-    gl:enable(?GL_CULL_FACE),
-    gl:disable(?GL_BLEND),
-    gl:depthMask(?GL_TRUE),
-    RS.
+    case Transp of
+        true ->
+            %% Render a alpha test pass for almost opaque fragments
+            gl:drawBuffer(?GL_NONE),
+            polygonOffset(3.0),
+            RS4 = wings_shaders:use_prog(alpha_test, RS3),
+            RS5 = render_smooth_objects_0(Open, true, false, RS4),
+            gl:enable(?GL_CULL_FACE),
+            RS6 = render_smooth_objects_0(Closed, true, false, RS5),
+            gl:drawBuffer(?GL_BACK),
+            gl:depthFunc(?GL_LEQUAL),
+            polygonOffset(2.0),
+            RS8 = enable_lighting(SceneLights, RS6),
+            gl:enable(?GL_BLEND),
+            gl:blendFunc(?GL_SRC_ALPHA, ?GL_ONE_MINUS_SRC_ALPHA),
+            gl:depthMask(?GL_FALSE),
+            RS9 = render_smooth_objects_0(Closed, true, SceneLights, RS8),
+            wings_pref:get_value(show_backfaces) andalso gl:disable(?GL_CULL_FACE),
+            RS10 = render_smooth_objects_0(Open, true, SceneLights, RS9),
+            RS = disable_lighting(RS10),
+            gl:enable(?GL_CULL_FACE),
+            gl:disable(?GL_BLEND),
+            gl:depthMask(?GL_TRUE),
+            RS;
+        false ->
+            RS3
+    end.
 
 render_smooth_objects_0([], _, _, RS0) ->
     RS0;
@@ -296,8 +299,6 @@ render_object_1(#dlo{mirror=MirrorMat}=DL, Work, RenderTrans, SceneLights, RS0) 
 
 render_object_2(D, true, _, SceneLights, RS) ->
     render_plain(D, SceneLights, RS);
-render_object_2(#dlo{transparent=true}=D, false, false, SceneLights, RS) ->
-    render_smooth(D, false, SceneLights, RS);
 render_object_2(D, false, RenderTrans, SceneLights, RS) ->
     render_smooth(D, RenderTrans, SceneLights, RS).
 
@@ -333,14 +334,12 @@ render_plain(#dlo{proxy_data=PD, drag=Drag}, SceneLights, RS0) ->
             wings_dl:call(Faces, RS0)
     end.
 
-render_smooth(#dlo{work=Work,smooth=Smooth0,transparent=Trans0,
-		   proxy=Proxy,proxy_data=PD},
+render_smooth(#dlo{work=Work,smooth=Smooth0, proxy=Proxy,proxy_data=PD},
 	      RenderTrans, _SceneLights, RS) ->
-    {Smooth, _Trans} =
-        case Proxy of
-            true -> wings_proxy:smooth_dl(PD);
-            false -> {Smooth0,Trans0}
-        end,
+    Smooth = case Proxy of
+                 false -> Smooth0;
+                 true -> element(1, wings_proxy:smooth_dl(PD))
+             end,
 
     Draw = case {Smooth,RenderTrans} of
                {none,false}   -> Work;
@@ -456,14 +455,14 @@ render_wire_object_1(#dlo{mirror=Matrix}=D, PStyle, RS0) ->
     gl:frontFace(?GL_CCW),
     RS.
 
-split_objects([#dlo{src_we=We}=D|Dls], Open, Closed, Lights) when ?IS_ANY_LIGHT(We) ->
-    split_objects(Dls, Open, Closed, [D|Lights]);
-split_objects([#dlo{open=true}=D|Dls], Open, Closed, Lights) ->
-    split_objects(Dls, [D|Open], Closed, Lights);
-split_objects([#dlo{open=false}=D|Dls], Open, Closed, Lights) ->
-    split_objects(Dls, Open, [D|Closed], Lights);
-split_objects([], Open, Closed, Lights) ->
-    {Open, Closed, Lights}.
+split_objects([#dlo{src_we=We}=D|Dls], Trans, Open, Closed, Lights) when ?IS_ANY_LIGHT(We) ->
+    split_objects(Dls, Trans, Open, Closed, [D|Lights]);
+split_objects([#dlo{open=true, transparent=T}=D|Dls], Trans, Open, Closed, Lights) ->
+    split_objects(Dls, T orelse Trans, [D|Open], Closed, Lights);
+split_objects([#dlo{open=false, transparent=T}=D|Dls], Trans, Open, Closed, Lights) ->
+    split_objects(Dls, T orelse Trans, Open, [D|Closed], Lights);
+split_objects([], Trans, Open, Closed, Lights) ->
+    {Trans, Open, Closed, Lights}.
 
 split_wires([#dlo{src_we=We}|Dls], WOs, Show, Wires, Others, Proxis) when ?IS_LIGHT(We) ->
     split_wires(Dls, WOs, Show, Wires, Others, Proxis);

shaders/alpha_test.fs
@@ -8,11 +8,10 @@
 #include "lib_base.glsl"
 #include "lib_material.glsl"
 
+uniform float alpha;
+
 void main(void)
 {
   vec4 basecolor = get_basecolor();
-  gl_FragColor = vec4(gl_FragCoord.z, basecolor.a, 0.0, 1.0);
-  if (basecolor.a < 0.8) {
-    discard;
-  }
+  if (basecolor.a < alpha) { discard; }
 }

src/wings_shaders.erl
@@ -26,15 +26,17 @@
 init() ->
     wings_pref:set_default(hl_skycol, {0.95,0.95,0.90}),
     wings_pref:set_default(hl_groundcol, {0.026,0.024,0.021}),
+    wings_pref:set_default(smooth_alpha, 0.850013),
     compile_all().
 
 compile_all() ->
     HL = [{'LightPosition', wings_pref:get_value(hl_lightpos)},
 	  {'SkyColor', wings_pref:get_value(hl_skycol)},
 	  {'GroundColor', wings_pref:get_value(hl_groundcol)}],
+    SA = [{'alpha', wings_pref:get_value(smooth_alpha)}],
     Programs0 = [{1, camera_light, [], "One Camera Lights"},
                  {2, hemilight, HL, "Hemispherical Lighting"},
-                 {alpha_test, alpha_test, [], ""},
+                 {alpha_test, alpha_test, SA, ""},
                  {ambient_light, ambient_light, [], ""},
                  {infinite_light, infinite_light, [], ""},
                  {point_light, point_light, [], ""},

commit b302aab10ad3188b0fd57c3965a38a1580b5c852

Fixup smooth-render without alpha
Broke it in 123cc7ff6a956836c2dd79c5e264ce780577f021

src/wings_render.erl
@@ -250,7 +250,9 @@ render_smooth_objects(Transp, Open, Closed, SceneLights, RS0) ->
             gl:depthMask(?GL_TRUE),
             RS;
         false ->
-            RS3
+            RS = disable_lighting(RS3),
+            gl:enable(?GL_CULL_FACE),
+            RS
     end.
 
 render_smooth_objects_0([], _, _, RS0) ->