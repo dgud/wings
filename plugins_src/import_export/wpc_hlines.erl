cb8269197640c23c16035d6696b0c0391de9c5b4
Dan Gudmundsson <dgud@users.sourceforge.net>
2019-03-12 06:58:00 +0100

    Remove explicit wm_freeze
    Always done in wings_export:export(..).

--- plugins_src/import_export/wpc_hlines.erl
+++ plugins_src/import_export/wpc_hlines.erl
@@ -131,7 +131,7 @@ export(Arg, Op, St) when is_atom(Arg) ->
             _ -> fun(Res) -> {file, {Op, {eps, Res}}} end
 	end,
     wpa:dialog(Arg, ?__(1,"Cartoon edges Render Options (EPS/SVG)"), dialog(), Fun);
-export(Arg, Op, St0) when is_list(Arg) ->
+export(Arg, Op, St) when is_list(Arg) ->
     set_pref(Arg),
     Camera_info = wpa:camera_info([aim, distance_to_aim,
 				   azimuth, elevation, tracking,
@@ -145,15 +145,9 @@ export(Arg, Op, St0) when is_list(Arg) ->
 	     {win_size, wings_wm:win_size()},
 	     {ortho_view, wings_wm:get_prop(orthogonal_view)}],
 
-    %% Freeze virtual mirrors.
-    Shapes0 = gb_trees:to_list(St0#st.shapes),
-    Shapes = [{Id, wpa:vm_freeze(We)} || {Id, We} <- Shapes0],
-    St = St0#st{shapes = gb_trees:from_orddict(Shapes)},
-
     case Op of
         export ->
-            ?SLOW(wpa:export(Props, fun_export(Props), St))
-      ;
+            ?SLOW(wpa:export(Props, fun_export(Props), St));
         export_selected ->
             ?SLOW(wpa:export_selected(Props, fun_export(Props), St))
     end;
