af6e932747e5777ce1a9b0e560b1ea63ea87130c
Edward Blake <mail@edwardlblake.com>
2025-01-20 12:29:36 -0500

    First release of the scripting plugin.
    Added set_face_attr_vs/4 to wings_va which can set new attribute values from
    the list returned by face_attr/3.
    
    Added replace_attr/3 to wings_va.
    
    Added new config files. Fixes to some types of values passed to the script.
    More distinct use of vector vs list in returned script data. New preference
    dialog implemented with help from micheus.
    
    Scripts can now use checkboxes and dropdown lists for parameters in the
    simple parameter layout. Scripts can also specify advanced parameter layouts
    as erlang terms directly.
    
    A lot of changes made to the scripting plugin. Added manual code,
    documentation and tools, updated makefiles, added lang file, fixes.
    
    NOTE: Added scripting to wings.

---
+++ plugins_src/scripting/docs/en/README.txt
@@ -0,0 +1,18 @@
+Scripting Plugin for Wings3D
+
+This reference contains three sections:
+
+Script Information (.wscr)
+
+Scheme Scripting
+
+Python Scripting
+
+
+CONTRIBUTORS FOR DOCUMENTATION
+
+Edward Blake has written the documentation for the first
+release of the scripting plugin.
+
+
+
---
+++ plugins_src/scripting/init_scripts/py/init.py
@@ -0,0 +1,128 @@
+
+##
+##  Scripting for Shapes  (Scheme and Python)
+##
+##  Copyright 2023-2025 Edward Blake
+##
+##  See the file "license.terms" for information on usage and redistribution
+##  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+##
+##     $Id$
+##
+
+##
+## This python code acts as a boot strap to define some useful functions
+## and then load the actual script that is sent as a symbolic expression through
+## standard input by the erlang plugin.
+##
+
+import w3d_int
+import sys
+import time
+from os import path
+from runpy import run_path
+
+
+sys.stdin.reconfigure(encoding="utf-8")
+sys.stdout.reconfigure(encoding="utf-8")
+sys.stderr.reconfigure(encoding="utf-8")
+
+scr_langstrs = {}
+
+def set_lang_strings(l):
+    global scr_langstrs
+    for a in l:
+        scr_langstrs[a[0]] = a[1]
+
+def __t(id, str):
+    global scr_langstrs
+    if id in scr_langstrs:
+        return scr_langstrs[id]
+    else:
+        return str
+
+def script_loader_loop():
+    # Main function to call, can be called many times
+    scr_main_fun = None
+    while True:
+        while True:
+            line = sys.stdin.readline()
+            if (line == ""):
+                pass
+            cmd_0, _ = w3d_int.scm_parse(line)
+            cmd = cmd_0[0]
+            break
+        
+        cmd_atom = cmd[0]
+        if cmd_atom == "run_init":
+            g = {}
+            g["scm_parse"] = w3d_int.scm_parse
+            g["OutputList"] = w3d_int.OutputList
+            g["Okay"] = w3d_int.Okay
+            ## Use __t(20, "Text") for localized text
+            g["__t"] = __t
+            script_file = cmd[1]
+            set_lang_strings(cmd[2])
+            sys.path.insert(0, path.dirname(script_file))
+            g2 = run_path(script_file, g)
+            scr_main_fun = None
+            if "w3d_main_function" in g2:
+                scr_main_fun = g2["w3d_main_function"]
+            if scr_main_fun == None:
+                sys.stderr.write("ERROR: main function not set\n")
+                exit(1)
+            print("(ok)")
+            sys.stdout.flush()
+            
+        elif cmd_atom == "run":
+            ## Parameters being passed to the script from the script's
+            ## parameter options window.
+            params = []
+
+            ## Access parameters by key, for those that used an atom key
+            params_by_key = {}
+
+            ## Extra parameters passed as the third argument
+            ## These are parameters that are not set via a parameter
+            ## window and may be auxiliary variables dependent on the
+            ## type of script (e.g. "content" contains an e3d_file tuple
+            ## for exporter plugins, parameters set with "script_params"
+            ## also show up in here).
+            extra_params = {}
+
+            params = cmd[2]
+
+            ## Add to params_by_key parameters that are keyed
+            for p in params:
+                if hasattr(p, "__len__"):
+                    if len(p) == 2:
+                        pv = p[1]
+                        params_by_key[p[0]] = pv
+
+            ## Grab the extra params
+            extra_params_1 = cmd[3]
+            for p in extra_params_1:
+                extra_params[p[0]] = p[1]
+            returned = scr_main_fun(params, params_by_key, extra_params)
+            if returned != None:
+                print("")
+                o = None
+                if hasattr(returned,"as_output_list"):
+                    o = returned.as_output_list()
+                elif hasattr(returned,"__len__"):
+                    o = w3d_int.OutputList()
+                    for r in returned:
+                        o.add_value(r)
+                if o != None:
+                    o.write_list_out(sys.stdout)
+            print("")
+            sys.stdout.flush()
+            print("(%ok)")
+            sys.stdout.flush()
+            
+        else:
+            print("ERROR: Not run")
+            exit(1)
+
+script_loader_loop()
+
